7f7bdc25bd9d9fbe577740c0cab0cc9b1bc49660
iizuka <iizuka@alinous.org>
2018-04-19 20:37:08 +0900

    File IO test

---
+++ src_test/db_base_io_stream/test_file_stream.cpp
@@ -0,0 +1,26 @@
+/*
+ * test_file_stream.cpp
+ *
+ *  Created on: 2018/04/19
+ *      Author: iizuka
+ */
+
+
+#include "CppUTest/CommandLineTestRunner.h"
+#include "test_utils/TestSetup.h"
+
+using namespace alinous;
+
+
+TEST_GROUP(FileStreamTestGroup) {
+	TestSetup testenv;
+
+	TEST_SETUP() {
+		testenv.setup();
+	}
+	TEST_TEARDOWN() {
+		testenv.teardown();
+	}
+
+};
+
