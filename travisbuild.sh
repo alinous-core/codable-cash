1c780f7de67fb6bca765fe1c22cd5787d7a10e8c
iizuka <iizuka@alinous.org>
2018-05-02 18:18:47 +0900

    changed setting travis.yml

--- travisbuild.sh
+++ travisbuild.sh
@@ -9,7 +9,8 @@ mkdir target
 
 pushd target
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-5 -DCMAKE_C_COMPILER=gcc-5 ..
+#cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-5 -DCMAKE_C_COMPILER=gcc-5 ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 
 make all
 # make test #ARGS='-V'
