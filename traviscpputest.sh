f87d7be1e7d4184554e08bba1ba8cb39b846d5da
iizuka <iizuka@alinous.org>
2018-05-02 21:03:15 +0900

    travis

---
+++ traviscpputest.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo 'Build latest CppUTest'
+set -e
+
+mkdir cpputest
+cd cpputest
+
+git clone https://github.com/cpputest/cpputest.git
+
+cd cpputest
+git checkout v3.8
+cmake CMakeLists.txt
+make
+cd ../..
+
+
