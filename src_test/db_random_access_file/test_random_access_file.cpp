3a2c38f8fde758c90c5c6c06fcd934f3ccd020d5
iizuka <iizuka@alinous.org>
2018-05-06 00:01:22 +0900

    printf debug

--- src_test/db_random_access_file/test_random_access_file.cpp
+++ src_test/db_random_access_file/test_random_access_file.cpp
@@ -385,7 +385,7 @@ TEST(RAFTestGroup, pagesizeChange){
 	char* buff = new char[8];
 	uint64_t fpos = 12;
 	file.read(fpos, buff, 8);
-	file.read(fpos, buff, 8);
+	//file.read(fpos, buff, 8);
 
 	delete [] buff;
 
