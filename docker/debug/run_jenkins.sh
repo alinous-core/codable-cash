45e093ae13f2653ca114e2bdb68b485ea04b220a
iizuka <iizuka@alinous.org>
2018-05-12 20:07:06 +0900

    run jenkins

---
+++ docker/debug/run_jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pushd ../..
+
+projectSrc=$(pwd)
+echo $projectSrc
+
+popd
+
+# t transfer host input to container
+# i open stdout
+# d background
+# --name test-dev
+docker container run --rm --privileged\
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v $projectSrc:/var/codablecash \
+	 ubuntu16 \
+	/var/make.sh $(whoami)
