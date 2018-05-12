388603281368ac9564b48e64a9885683ed1fec5d
iizuka <iizuka@alinous.org>
2018-05-12 18:38:05 +0900

    --privileged

--- docker/debug/run.sh
+++ docker/debug/run.sh
@@ -11,7 +11,7 @@ popd
 # i open stdout
 # d background
 # --name test-dev
-docker container run --rm \
+docker container run --rm --privileged\
 	-v $projectSrc:/var/codablecash \
 	 ubuntu16 \
 	/var/make.sh
