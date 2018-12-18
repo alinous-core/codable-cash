5d241f5c0b2ed092fd1fb57b4029dae5b631fb1c
iizuka <iizuka@alinous.org>
2018-12-18 21:04:13 +0900

    btree folder

---
+++ src_db/btree/Btree.cpp
@@ -0,0 +1,21 @@
+/*
+ * Btree.cpp
+ *
+ *  Created on: 2018/12/18
+ *      Author: iizuka
+ */
+
+#include <btree/Btree.h>
+
+namespace alinous {
+
+Btree::Btree() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Btree::~Btree() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace alinous */
---
+++ src_db/btree/Btree.h
@@ -0,0 +1,21 @@
+/*
+ * Btree.h
+ *
+ *  Created on: 2018/12/18
+ *      Author: iizuka
+ */
+
+#ifndef BTREE_BTREE_H_
+#define BTREE_BTREE_H_
+
+namespace alinous {
+
+class Btree {
+public:
+	Btree();
+	virtual ~Btree();
+};
+
+} /* namespace alinous */
+
+#endif /* BTREE_BTREE_H_ */
