6b07f0d637cace588d944f65b44fac71bbb1e9c0
iizuka <iizuka@alinous.org>
2019-01-15 18:03:12 +0900

    tokenizer

--- src_smartcontract/sc/SmartContractParser.cpp
+++ src_smartcontract/sc/SmartContractParser.cpp
@@ -6,11 +6,16 @@
  */
 
 #include "sc/SmartContractParser.h"
-#include "base_io/File.h"
-#include "base_io_stream/FileInputStream.h"
 #include "sc/ParserReaderStream.h"
 
+#include "base_io/File.h"
+#include "base_io_stream/FileInputStream.h"
+
+#include "alinous_lang/CharStream.h"
+#include "alinous_lang/AlinousLangTokenManager.h"
+
 namespace codablecash {
+using namespace alinouslang;
 
 SmartContractParser::SmartContractParser(const File* file) {
 	this->file = new File(*file);
@@ -24,7 +29,18 @@ void SmartContractParser::parse() {
 	int length = this->file->length();
 	FileInputStream inStream(this->file);
 
+	inStream.open();
+
 	ParserReaderStream readStream(&inStream, length);
+	CharStream charStream(&readStream);
+
+	AlinousLangTokenManager tokenManager(&charStream);
+
+	Token* t = tokenManager.getNextToken();
+	delete t;
+
+	t = tokenManager.getNextToken();
+	delete t;
 
 }
 
