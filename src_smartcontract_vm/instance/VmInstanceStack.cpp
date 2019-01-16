8bc24686213e1eb90bd1c22b759fad0b3f1c96a8
iizuka <iizuka@alinous.org>
2019-01-16 22:28:36 +0900

    mock of stack

---
+++ src_smartcontract_vm/instance/VmInstanceStack.cpp
@@ -0,0 +1,21 @@
+/*
+ * VmInstanceStack.cpp
+ *
+ *  Created on: 2019/01/16
+ *      Author: iizuka
+ */
+
+#include <instance/VmInstanceStack.h>
+
+namespace alinous {
+
+VmInstanceStack::VmInstanceStack() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VmInstanceStack::~VmInstanceStack() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace alinous */
---
+++ src_smartcontract_vm/instance/VmInstanceStack.h
@@ -0,0 +1,21 @@
+/*
+ * VmInstanceStack.h
+ *
+ *  Created on: 2019/01/16
+ *      Author: iizuka
+ */
+
+#ifndef INSTANCE_VMINSTANCESTACK_H_
+#define INSTANCE_VMINSTANCESTACK_H_
+
+namespace alinous {
+
+class VmInstanceStack {
+public:
+	VmInstanceStack();
+	virtual ~VmInstanceStack();
+};
+
+} /* namespace alinous */
+
+#endif /* INSTANCE_VMINSTANCESTACK_H_ */
