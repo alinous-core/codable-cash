32dfb32a82c5cb5f35c6c65d33fb6fcd1863abc6
iizuka <iizuka@alinous.org>
2019-01-19 14:25:23 +0900

    added test

---
+++ src_test/smartcontract/test_class_fw.cpp
@@ -0,0 +1,18 @@
+/*
+ * test_class_fw.cpp
+ *
+ *  Created on: 2019/01/19
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+TEST_GROUP(TestClassFwGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+
+};
+
+TEST(TestClassFwGroup, construct){
+
+}
