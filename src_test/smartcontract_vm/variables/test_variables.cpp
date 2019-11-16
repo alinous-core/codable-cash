ecf5687dd32a51d2c8b7613b4ebcf0da7e21a1bc
iizuka <iizuka@alinous.org>
2019-11-16 21:59:41 +0900

    substitute primitive values

---
+++ src_test/smartcontract_vm/variables/test_variables.cpp
@@ -0,0 +1,47 @@
+/*
+ * test_variables.cpp
+ *
+ *  Created on: 2019/11/16
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+#include "base_io_stream/FileInputStream.h"
+
+using namespace alinous;
+
+
+TEST_GROUP(TestVmVariableGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestVmVariableGroup, construct){
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_vm/variables/resources/intlong/main.alns"));
+
+	SmartContract* sc = new SmartContract();
+	FileInputStream stream(sourceFile);
+
+	int length = sourceFile->length();
+	sc->addCompilationUnit(&stream, length);
+
+	UnicodeString mainPackage(L"test.fw");
+	UnicodeString mainClass(L"SmartContract");
+	UnicodeString mainMethod(L"main");
+	sc->setMainMethod(&mainPackage, &mainClass, &mainMethod);
+
+	VirtualMachine* vm = new VirtualMachine(1024*1024); __STP(vm);
+	vm->loadSmartContract(sc);
+
+	vm->analyze();
+	vm->createScInstance();
+	vm->destroy();
+}
+
+
+
+
