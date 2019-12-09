eaddd5e481c40e9a512253bf0f56e70b9b614047
iizuka <iizuka@alinous.org>
2019-12-09 20:55:41 +0900

    testing function call mock

---
+++ src_test/smartcontract_vm/method_invoke/test_function_call.cpp
@@ -0,0 +1,26 @@
+/*
+ * test_function_call.cpp
+ *
+ *  Created on: 2019/12/09
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+
+using namespace alinous;
+
+TEST_GROUP(TestFunctionCallGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestFunctionCallGroup, callMainMethod){
+
+}
+
