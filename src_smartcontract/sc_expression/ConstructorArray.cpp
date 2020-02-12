f59d70c849fa561791d333a2d573c685ed0d3df7
iizuka <iizuka@alinous.org>
2020-02-12 19:23:17 +0900

    Base of virtual functions of ConstructorArray

--- src_smartcontract/sc_expression/ConstructorArray.cpp
+++ src_smartcontract/sc_expression/ConstructorArray.cpp
@@ -31,19 +31,67 @@ ConstructorArray::~ConstructorArray() {
 }
 
 int ConstructorArray::binarySize() const {
+	checkNotNull(this->valId);
+
+	int total = sizeof(uint16_t);
+	total += this->valId->binarySize();
+
+	int maxLoop = this->dims.size();
+	total += sizeof(int32_t);
+
+	for(int i = 0; i != maxLoop; ++i){
+		NumberLiteral* lit = this->dims.get(i);
+		total += lit->binarySize();
+	}
+
+	return total;
 }
 
 void ConstructorArray::toBinary(ByteBuffer* out) {
 	checkNotNull(this->valId);
+
+	out->putShort(CodeElement::EXP_CONSTRUCTORARRAY);
+
+	int maxLoop = this->dims.size();
+	for(int i = 0; i != maxLoop; ++i){
+		NumberLiteral* lit = this->dims.get(i);
+		lit->toBinary(out);
+	}
 }
 
 void ConstructorArray::fromBinary(ByteBuffer* in) {
+	CodeElement* element = createFromBinary(in);
+	checkKind(element, CodeElement::EXP_VARIABLE_ID);
+	this->valId = dynamic_cast<VariableIdentifier*>(element);
+
+	int maxLoop = in->getInt();
+	for(int i = 0; i != maxLoop; ++i){
+		element = createFromBinary(in);
+		checkKind(element, CodeElement::EXP_NUMBER_LITERAL);
+
+		NumberLiteral* lit = dynamic_cast<NumberLiteral*>(element);
+		this->dims.addElement(lit);
+	}
 }
 
 void ConstructorArray::preAnalyze(AnalyzeContext* actx) {
+	this->valId->preAnalyze(actx);
+
+	int maxLoop = this->dims.size();
+	for(int i = 0; i != maxLoop; ++i){
+		NumberLiteral* lit = this->dims.get(i);
+		lit->preAnalyze(actx);
+	}
 }
 
 void ConstructorArray::analyzeTypeRef(AnalyzeContext* actx) {
+	this->valId->analyzeTypeRef(actx);
+
+	int maxLoop = this->dims.size();
+	for(int i = 0; i != maxLoop; ++i){
+		NumberLiteral* lit = this->dims.get(i);
+		lit->analyzeTypeRef(actx);
+	}
 }
 
 void ConstructorArray::analyze(AnalyzeContext* actx) {
@@ -53,6 +101,7 @@ AnalyzedType ConstructorArray::getType(AnalyzeContext* actx) {
 }
 
 void ConstructorArray::init(VirtualMachine* vm) {
+	this->valId->init(vm);
 }
 
 AbstractVmInstance* ConstructorArray::interpret(VirtualMachine* vm) {
