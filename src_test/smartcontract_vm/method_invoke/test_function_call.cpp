68af3ab1ad934498ff11205ef64dac809e20f57b
iizuka <iizuka@alinous.org>
2020-02-25 17:08:20 +0900

    updated test bench

--- src_test/smartcontract_vm/method_invoke/test_function_call.cpp
+++ src_test/smartcontract_vm/method_invoke/test_function_call.cpp
@@ -48,5 +48,12 @@ TEST(TestFunctionCallGroup, callMainMethod){
 }
 
 TEST(TestFunctionCallGroup, returnvalue){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/method_invoke/resources/methods/invoke01/", projectFolder);
 
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
 }
