36eeae99c199dc7ce6c81b051ac6508d2baea542
iizuka <iizuka@alinous.org>
2020-03-02 16:16:30 +0900

    array dimension check

--- src_smartcontract_vm/type_check/InternalTypeChecker.cpp
+++ src_smartcontract_vm/type_check/InternalTypeChecker.cpp
@@ -5,12 +5,27 @@
  *      Author: iizuka
  */
 
+#include <cstdint>
+
 #include "type_check/InternalTypeChecker.h"
+
+#include "sc_analyze/AnalyzedType.h"
 
 namespace alinous {
 
 
 int InternalTypeChecker::analyzeCompatibility(AnalyzedType* leftType, AnalyzedType* rightType) {
+	uint8_t leftTypeCode = leftType->getType();
+	int dim = leftType->getDim();
+
+	if(dim != rightType->getDim()){
+		return INCOMPATIBLE;
+	}
+
+	switch(leftTypeCode){
+
+	}
+
 	// FIXME type check
 	return 0;
 
