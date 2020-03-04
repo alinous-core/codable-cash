5e7e29f70ca757a24f311fa0f996b9817cf7351b
iizuka <iizuka@alinous.org>
2020-03-04 17:31:48 +0900

    test program

--- src_test/smartcontract_vm/base/test_interface.cpp
+++ src_test/smartcontract_vm/base/test_interface.cpp
@@ -26,6 +26,13 @@ TEST_GROUP(TestInterfaceGroup) {
 };
 
 TEST(TestInterfaceGroup, base01){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/base/resources/interface/case01/", projectFolder);
 
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
 }
 
