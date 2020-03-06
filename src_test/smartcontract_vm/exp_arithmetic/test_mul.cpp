6563e51afffd4b279cd3f3da40d55d2b9c297542
iizuka <iizuka@alinous.org>
2020-03-06 15:51:28 +0900

    added test group

---
+++ src_test/smartcontract_vm/exp_arithmetic/test_mul.cpp
@@ -0,0 +1,33 @@
+/*
+ * test_mul.cpp
+ *
+ *  Created on: 2020/03/06
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+#include "base/UnicodeString.h"
+
+
+using namespace alinous;
+
+
+TEST_GROUP(TestMulGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestMulGroup, case01){ // int
+
+}
+
