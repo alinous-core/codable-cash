13be4bb5891a47b5ff58ba97a0fb3b4ab9ea1cc9
iizuka <iizuka@alinous.org>
2020-03-07 20:15:12 +0900

    interpret16Bit()

--- src_test/smartcontract_vm/exp_arithmetic/test_post_inc.cpp
+++ src_test/smartcontract_vm/exp_arithmetic/test_post_inc.cpp
@@ -130,3 +130,56 @@ TEST(TestPostIncGroup, case02_m){
 
 //////////////////////////////////
 
+TEST(TestPostIncGroup, case03){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/exp_arithmetic/resources/postinc/case03/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	UnicodeString strCount(L"count");
+	ExtPrimitiveObject* count = obj->getExtPrimitiveObject(&strCount);
+
+	CHECK(count->getShortValue() == 2);
+
+	UnicodeString strLastCount(L"lastcount");
+	ExtPrimitiveObject* lastcount = obj->getExtPrimitiveObject(&strCount);
+
+	CHECK(count->getShortValue() == 2);
+}
+
+TEST(TestPostIncGroup, case03_m){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/exp_arithmetic/resources/postinc/case03_m/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	UnicodeString strCount(L"count");
+	ExtPrimitiveObject* count = obj->getExtPrimitiveObject(&strCount);
+
+	CHECK(count->getShortValue() == 0);
+
+	UnicodeString strLastCount(L"lastcount");
+	ExtPrimitiveObject* lastcount = obj->getExtPrimitiveObject(&strCount);
+
+	CHECK(count->getShortValue() == 0);
+}
+
+//////////////////////////////////
+
+
