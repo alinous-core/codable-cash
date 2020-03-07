ef5e2cf22a5eefbe5390b0df3e647555c91f9543
iizuka <iizuka@alinous.org>
2020-03-07 12:18:07 +0900

    test bench of preinc

---
+++ src_test/smartcontract_vm/exp_arithmetic/test_pre_inc.cpp
@@ -0,0 +1,38 @@
+/*
+ * test_pre_inc.cpp
+ *
+ *  Created on: 2020/03/07
+ *      Author: iizuka
+ */
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+#include "base/UnicodeString.h"
+
+
+using namespace alinous;
+
+TEST_GROUP(TestPreIncGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestPreIncGroup, case01){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/exp_arithmetic/resources/preinc/case01/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+}
+
