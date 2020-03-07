31f4ddfb55b7b62829e3f2e94ed1b370b65effd6
iizuka <iizuka@alinous.org>
2020-03-07 16:01:59 +0900

    Type error case

--- src_test/smartcontract_vm/exp_arithmetic/test_pre_inc.cpp
+++ src_test/smartcontract_vm/exp_arithmetic/test_pre_inc.cpp
@@ -232,3 +232,14 @@ TEST(TestPreIncGroup, case04_m){
 }
 
 /////////////////////////////////////////////////////////////////////////////
+
+TEST(TestPreIncGroup, case05){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/exp_arithmetic/resources/preinc/case05/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
