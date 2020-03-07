4fdf6a1f3f4f11d4629951a850bb00adf88d9e63
iizuka <iizuka@alinous.org>
2020-03-07 18:29:26 +0900

    added test program & folder

---
+++ src_test/smartcontract_vm/exp_arithmetic/test_post_inc.cpp
@@ -0,0 +1,33 @@
+/*
+ * test_post_inc.cpp
+ *
+ *  Created on: 2020/03/07
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+#include "base/UnicodeString.h"
+
+
+using namespace alinous;
+
+TEST_GROUP(TestPostIncGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestPostIncGroup, case01){
+
+}
+
+
