1c8949b58da079d977eae4c6bfa5fcb35f6f9213
iizuka <iizuka@alinous.org>
2020-03-08 23:33:16 +0900

    Equality test bench

---
+++ src_test/smartcontract_vm/exp_logical/test_equality_exp.cpp
@@ -0,0 +1,32 @@
+/*
+ * test_equality_exp.cpp
+ *
+ *  Created on: 2020/03/08
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+#include "base/UnicodeString.h"
+
+
+using namespace alinous;
+
+
+TEST_GROUP(TestEqualityGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestEqualityGroup, case01){
+
+}
