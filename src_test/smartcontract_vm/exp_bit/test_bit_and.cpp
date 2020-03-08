7496d601c4b9137c5895aececfc969880980a067
iizuka <iizuka@alinous.org>
2020-03-08 20:34:52 +0900

    TestBitAndGroup case01

--- src_test/smartcontract_vm/exp_bit/test_bit_and.cpp
+++ src_test/smartcontract_vm/exp_bit/test_bit_and.cpp
@@ -26,8 +26,24 @@ TEST_GROUP(TestBitAndGroup) {
 	TEST_TEARDOWN(){}
 };
 
-TEST(TestBitAndGroup, case01){
+TEST(TestBitAndGroup, case01){ // int
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/exp_bit/resources/and/case01/", projectFolder);
 
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	UnicodeString strCount(L"count");
+	ExtPrimitiveObject* count = obj->getExtPrimitiveObject(&strCount);
+
+	CHECK(count->getIntValue() == 9);
 }
 
 
