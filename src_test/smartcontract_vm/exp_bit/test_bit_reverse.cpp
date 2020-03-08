d946a510c9168f9bf6f4241500f8b6b27c820c92
iizuka <iizuka@alinous.org>
2020-03-08 21:10:23 +0900

    type error case

--- src_test/smartcontract_vm/exp_bit/test_bit_reverse.cpp
+++ src_test/smartcontract_vm/exp_bit/test_bit_reverse.cpp
@@ -107,4 +107,14 @@ TEST(TestBitReverseGroup, case04){ // long
 	CHECK(count->getLongValue() == -1);
 }
 
+TEST(TestBitReverseGroup, case05){ // error
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/exp_bit/resources/brev/case05/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
 
