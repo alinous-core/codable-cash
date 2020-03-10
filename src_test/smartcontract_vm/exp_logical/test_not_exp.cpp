678b1bbf541d943a70fb6ef6cb4b4667e3fed173
iizuka <iizuka@alinous.org>
2020-03-10 18:04:51 +0900

    test NotExpression

--- src_test/smartcontract_vm/exp_logical/test_not_exp.cpp
+++ src_test/smartcontract_vm/exp_logical/test_not_exp.cpp
@@ -70,4 +70,14 @@ TEST(TestNotGroup, case02){
 	CHECK(oresult->getBoolValue() == false);
 }
 
+TEST(TestNotGroup, case03){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/exp_logical/resources/not/case03/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
 
