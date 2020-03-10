2220a931f6cbc8c34773f83e0ed6d4c1588b1b57
iizuka <iizuka@alinous.org>
2020-03-10 17:33:48 +0900

    made testbench and test case file

---
+++ src_test/smartcontract_vm/exp_logical/test_not_exp.cpp
@@ -0,0 +1,34 @@
+/*
+ * test_not_exp.cpp
+ *
+ *  Created on: 2020/03/10
+ *      Author: iizuka
+ */
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+#include "base/UnicodeString.h"
+
+
+using namespace alinous;
+
+
+TEST_GROUP(TestNotGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestNotGroup, case01){
+
+}
+
+
+
