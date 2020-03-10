f9431d43884425ae5733b5919a86f49822990dba
iizuka <iizuka@alinous.org>
2020-03-10 18:27:44 +0900

    added test files

---
+++ src_test/smartcontract_vm/exp_logical/test_logical_and.cpp
@@ -0,0 +1,10 @@
+/*
+ * test_logical_and.cpp
+ *
+ *  Created on: 2020/03/10
+ *      Author: iizuka
+ */
+
+
+
+
---
+++ src_test/smartcontract_vm/exp_logical/test_logical_or.cpp
@@ -0,0 +1,10 @@
+/*
+ * test_logical_or.cpp
+ *
+ *  Created on: 2020/03/10
+ *      Author: iizuka
+ */
+
+
+
+
