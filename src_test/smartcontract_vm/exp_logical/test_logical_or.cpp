91c49da8d3f03fbf7585b5eaeff4c994681125d0
iizuka <iizuka@alinous.org>
2020-03-10 18:37:09 +0900

    tested logical AND expression

--- src_test/smartcontract_vm/exp_logical/test_logical_or.cpp
+++ src_test/smartcontract_vm/exp_logical/test_logical_or.cpp
@@ -4,7 +4,30 @@
  *  Created on: 2020/03/10
  *      Author: iizuka
  */
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+#include "base/UnicodeString.h"
 
 
+using namespace alinous;
+
+
+TEST_GROUP(TestLogicalOrGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestLogicalOrGroup, case01){
+
+}
 
 
