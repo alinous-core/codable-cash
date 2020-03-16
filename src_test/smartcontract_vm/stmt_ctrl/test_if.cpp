a1accd6ea2e55eedcec5a43d8a3e1622cb879854
iizuka <iizuka@alinous.org>
2020-03-16 15:45:27 +0900

    if else if case

--- src_test/smartcontract_vm/stmt_ctrl/test_if.cpp
+++ src_test/smartcontract_vm/stmt_ctrl/test_if.cpp
@@ -83,3 +83,24 @@ TEST(TestIfStmtGroup, case03){
 
 	CHECK(iresult == 2)
 }
+
+TEST(TestIfStmtGroup, case04){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_ctrl/resources/if/case04/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int iresult = VmTestUtils::getIntMemberValue(obj, L"result");
+
+	CHECK(iresult == 3)
+}
+
+
