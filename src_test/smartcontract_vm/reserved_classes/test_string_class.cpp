0d467abfa58220bc6e4b96f4277467598c06cf78
iizuka <iizuka@alinous.org>
2020-03-17 17:45:54 +0900

    first test bench of string class

---
+++ src_test/smartcontract_vm/reserved_classes/test_string_class.cpp
@@ -0,0 +1,26 @@
+/*
+ * test_string_class.cpp
+ *
+ *  Created on: 2020/03/17
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+
+using namespace alinous;
+
+TEST_GROUP(TestStringClassGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestStringClassGroup, base01){
+
+}
+
