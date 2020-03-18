2ce89550af3ed4a830ecc68c9a4dc7dbd7be3ab4
iizuka <iizuka@alinous.org>
2020-03-18 17:45:17 +0900

    made testbench

---
+++ src_test/smartcontract_vm/exp_lit/test_string_literal.cpp
@@ -0,0 +1,34 @@
+/*
+ * test_string_literal.cpp
+ *
+ *  Created on: 2020/03/18
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+#include "base/UnicodeString.h"
+
+
+using namespace alinous;
+
+
+TEST_GROUP(TestStringLiteralGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestStringLiteralGroup, case01){
+
+}
+
+
