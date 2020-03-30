40eabbae94048ed89adb478767c8be928c96a648
iizuka <iizuka@alinous.org>
2020-03-30 22:53:26 +0900

    error case

--- src_test/smartcontract_vm/variables/test_allocation_arg.cpp
+++ src_test/smartcontract_vm/variables/test_allocation_arg.cpp
@@ -110,4 +110,14 @@ TEST(TestAllocationArgGroup, objectArg03){ // null
 	CHECK(objectB == nullptr)
 }
 
+TEST(TestAllocationArgGroup, objectArg04_err){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/variables/resources/new_arg/case05/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
 
