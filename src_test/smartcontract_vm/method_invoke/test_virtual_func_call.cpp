360bf7d096fcf94d4e0337b9fe0e2eae866d09e9
iizuka <iizuka@alinous.org>
2020-04-05 16:15:03 +0900

    first vfunc test2

--- src_test/smartcontract_vm/method_invoke/test_virtual_func_call.cpp
+++ src_test/smartcontract_vm/method_invoke/test_virtual_func_call.cpp
@@ -42,3 +42,24 @@ TEST(TestVFunctionCallGroup, test01){
 
 	CHECK(param = 20);
 }
+
+TEST(TestVFunctionCallGroup, test02){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/method_invoke/resources/vfunc/case02/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int param = VmTestUtils::getIntMemberValue(obj, L"count");
+
+	CHECK(param = 20);
+}
+
+
