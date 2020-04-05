50cadb5ea81cd7f75e47387c133f402f64444c14
iizuka <iizuka@alinous.org>
2020-04-05 16:52:14 +0900

    Control manager

---
+++ src_smartcontract_vm/vm_ctrl/ExecControlManager.cpp
@@ -0,0 +1,21 @@
+/*
+ * ExecControlManager.cpp
+ *
+ *  Created on: 2020/04/05
+ *      Author: iizuka
+ */
+
+#include "vm_ctrl/ExecControlManager.h"
+
+namespace alinous {
+
+ExecControlManager::ExecControlManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ExecControlManager::~ExecControlManager() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace alinous */
---
+++ src_smartcontract_vm/vm_ctrl/ExecControlManager.h
@@ -0,0 +1,21 @@
+/*
+ * ExecControlManager.h
+ *
+ *  Created on: 2020/04/05
+ *      Author: iizuka
+ */
+
+#ifndef VM_CTRL_EXECCONTROLMANAGER_H_
+#define VM_CTRL_EXECCONTROLMANAGER_H_
+
+namespace alinous {
+
+class ExecControlManager {
+public:
+	ExecControlManager();
+	virtual ~ExecControlManager();
+};
+
+} /* namespace alinous */
+
+#endif /* VM_CTRL_EXECCONTROLMANAGER_H_ */
