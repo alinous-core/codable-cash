c16e83d03d9de425fd4bc24d6cb84444166bc16e
iizuka <iizuka@alinous.org>
2020-04-05 15:51:59 +0900

    initial test bench of Virtual function test

--- src_test/smartcontract_vm/method_invoke/test_function_call.cpp
+++ src_test/smartcontract_vm/method_invoke/test_function_call.cpp
@@ -101,4 +101,15 @@ TEST(TestFunctionCallGroup, constructor01_error){
 	CHECK(!result)
 }
 
+TEST(TestFunctionCallGroup, compile_error){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/method_invoke/resources/methods/compile_error/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
+
 
---
+++ src_test/smartcontract_vm/method_invoke/test_virtual_func_call.cpp
@@ -0,0 +1,36 @@
+/*
+ * test_virtual_func_call.cpp
+ *
+ *  Created on: 2020/04/03
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "base/StackRelease.h"
+
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+using namespace alinous;
+
+TEST_GROUP(TestVFunctionCallGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestVFunctionCallGroup, test01){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/method_invoke/resources/vfunc/case01/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+}
