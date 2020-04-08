3120da5335d5a821d5dbe4249b01852910cc37e0
iizuka <iizuka@alinous.org>
2020-04-08 20:54:40 +0900

    first while stetement test

--- src_test/smartcontract_vm/stmt_ctrl/test_while_stmt.cpp
+++ src_test/smartcontract_vm/stmt_ctrl/test_while_stmt.cpp
@@ -28,5 +28,20 @@ TEST_GROUP(TestWhileStmtGroup) {
 };
 
 TEST(TestWhileStmtGroup, case01){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_ctrl/resources/while/case01/", projectFolder);
 
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int iresult = VmTestUtils::getIntMemberValue(obj, L"result");
+
+	CHECK(iresult == 10)
 }
