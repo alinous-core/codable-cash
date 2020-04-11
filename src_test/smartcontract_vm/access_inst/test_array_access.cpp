bcfce23d63ffcfc187323e6558e7260b7c16ffe8
iizuka <iizuka@alinous.org>
2020-04-11 20:27:12 +0900

    test array 2D reference

--- src_test/smartcontract_vm/access_inst/test_array_access.cpp
+++ src_test/smartcontract_vm/access_inst/test_array_access.cpp
@@ -44,3 +44,21 @@ TEST(TestArrayAccessGroup, case01){
 	int iresult = VmTestUtils::getIntMemberValue(obj, L"result");
 	CHECK(iresult == 10);
 }
+
+TEST(TestArrayAccessGroup, case02){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/access_inst/resources/array/case02/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int iresult = VmTestUtils::getIntMemberValue(obj, L"result");
+	CHECK(iresult == 10);
+}
