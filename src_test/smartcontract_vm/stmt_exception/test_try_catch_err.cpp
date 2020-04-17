b573272b5bc3ef2ab607ab69e00b480f7d3db2a7
iizuka <iizuka@alinous.org>
2020-04-17 14:43:18 +0900

    throw exception error case

--- src_test/smartcontract_vm/stmt_exception/test_try_catch_err.cpp
+++ src_test/smartcontract_vm/stmt_exception/test_try_catch_err.cpp
@@ -51,3 +51,25 @@ TEST(TestTryCatchErrGroup, case02){
 	CHECK(!result)
 }
 
+TEST(TestTryCatchErrGroup, case03){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_exception/resources/try_err/case03/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
+
+TEST(TestTryCatchErrGroup, case04){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_exception/resources/try_err/case04/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
+
