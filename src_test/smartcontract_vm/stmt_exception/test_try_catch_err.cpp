5f2b85a1b3b73e1228cd6553f5e8ac27a5453598
iizuka <iizuka@alinous.org>
2020-04-17 14:39:54 +0900

    catch block error

---
+++ src_test/smartcontract_vm/stmt_exception/test_try_catch_err.cpp
@@ -0,0 +1,53 @@
+/*
+ * test_try_catch_err.cpp
+ *
+ *  Created on: 2020/04/17
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+#include "ext_binary/ExtExceptionObject.h"
+
+#include "base/UnicodeString.h"
+
+#include "sc/CodeElement.h"
+
+using namespace alinous;
+
+
+TEST_GROUP(TestTryCatchErrGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
+
+TEST(TestTryCatchErrGroup, case01){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_exception/resources/try_err/case01/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
+
+TEST(TestTryCatchErrGroup, case02){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_exception/resources/try_err/case02/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
+
