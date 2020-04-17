945a0cf65389a8d443caa25997552de30c1f00fc
iizuka <iizuka@alinous.org>
2020-04-17 14:30:33 +0900

    nest try catch

--- src_test/smartcontract_vm/stmt_exception/test_try_catch.cpp
+++ src_test/smartcontract_vm/stmt_exception/test_try_catch.cpp
@@ -67,3 +67,27 @@ TEST(TestTryCatchGroup, case02){
 	CHECK(result)
 }
 
+TEST(TestTryCatchGroup, case03){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_exception/resources/try/case03/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int iresult = VmTestUtils::getIntMemberValue(obj, L"result");
+
+	CHECK(iresult == 10)
+
+	ExtExceptionObject* exception = VmTestUtils::getExtExceptionObject(obj, L"e");
+	CodeElement* codeElement = exception->getCodeElement();
+
+	CHECK(codeElement->getKind() == CodeElement::STMT_THROW)
+}
+
