24061142156f132838b3f4d154d086d5b580494b
iizuka <iizuka@alinous.org>
2020-04-20 17:02:46 +0900

    array error

--- src_test/smartcontract_vm/access_inst/test_array_access.cpp
+++ src_test/smartcontract_vm/access_inst/test_array_access.cpp
@@ -72,7 +72,17 @@ TEST(TestArrayAccessGroup, case03_err){
 
 	bool result = util.analyze();
 	CHECK(!result)
+}
 
+TEST(TestArrayAccessGroup, case04_err){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/access_inst/resources/array/case04_err/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
 }
 
 
