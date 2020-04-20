b9331aebb2f79940862b8c70a56310b03e46e555
iizuka <iizuka@alinous.org>
2020-04-20 20:48:42 +0900

    TestAllocationStmtGroup

--- src_test/smartcontract_vm/variables/test_allocation.cpp
+++ src_test/smartcontract_vm/variables/test_allocation.cpp
@@ -162,3 +162,16 @@ TEST(TestAllocationStmtGroup, primitiveArrayAllocation07){
 	result = util.analyze();
 	CHECK(!result)
 }
+
+TEST(TestAllocationStmtGroup, primitiveArrayAllocation08){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/variables/resources/new/new08_err/", projectFolder);
+
+	bool result = util.loadAllFiles();
+	CHECK(result)
+
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	result = util.analyze();
+	CHECK(!result)
+}
