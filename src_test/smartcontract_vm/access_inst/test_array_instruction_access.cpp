c1f615bba590bde44df8f681ee58c2c4c1c57a50
iizuka <iizuka@alinous.org>
2020-04-21 16:28:54 +0900

    test workbench

--- src_test/smartcontract_vm/access_inst/test_array_instruction_access.cpp
+++ src_test/smartcontract_vm/access_inst/test_array_instruction_access.cpp
@@ -5,6 +5,31 @@
  *      Author: iizuka
  */
 
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+#include "sc/SmartContract.h"
+
+#include "base_io_stream/FileInputStream.h"
+
+#include "../VmTestUtils.h"
+#include "ext_binary/ExtClassObject.h"
+#include "ext_binary/ExtPrimitiveObject.h"
+
+#include "base/UnicodeString.h"
+
+#include "instance_exception/NullPointerExceptionClassDeclare.h"
+
+#include "instance_exception/ArrayOutOfBoundsExceptionClassDeclare.h"
+using namespace alinous;
 
 
+TEST_GROUP(TestArrayInstAccessGroup) {
+	TEST_SETUP(){}
+	TEST_TEARDOWN(){}
+};
 
+
+TEST(TestArrayInstAccessGroup, case01){
+
+}
