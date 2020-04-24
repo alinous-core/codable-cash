52327de99cf3e62aaab34c18b71ab143862a9e29
iizuka <iizuka@alinous.org>
2020-04-24 20:35:01 +0900

    boolean type check error

--- src_test/smartcontract_vm/exp/test_cast_exp.cpp
+++ src_test/smartcontract_vm/exp/test_cast_exp.cpp
@@ -91,3 +91,14 @@ TEST(TestCastExpGroup, case05){
 	CHECK(!result)
 }
 
+TEST(TestCastExpGroup, case06){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/exp/resources/cast/case06_err/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+}
+
