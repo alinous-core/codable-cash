5bb438757d15dd130c4b24cbf25eecf64756e705
iizuka <iizuka@alinous.org>
2020-04-28 22:56:29 +0900

    added ClassTypeAccess

---
+++ src_smartcontract_vm/variable_access/ClassTypeAccess.cpp
@@ -0,0 +1,20 @@
+/*
+ * ClassTypeAccess.cpp
+ *
+ *  Created on: 2020/04/28
+ *      Author: iizuka
+ */
+
+#include "variable_access/ClassTypeAccess.h"
+
+namespace alinous {
+
+ClassTypeAccess::ClassTypeAccess() {
+
+}
+
+ClassTypeAccess::~ClassTypeAccess() {
+
+}
+
+} /* namespace alinous */
---
+++ src_smartcontract_vm/variable_access/ClassTypeAccess.h
@@ -0,0 +1,21 @@
+/*
+ * ClassTypeAccess.h
+ *
+ *  Created on: 2020/04/28
+ *      Author: iizuka
+ */
+
+#ifndef VARIABLE_ACCESS_CLASSTYPEACCESS_H_
+#define VARIABLE_ACCESS_CLASSTYPEACCESS_H_
+
+namespace alinous {
+
+class ClassTypeAccess {
+public:
+	ClassTypeAccess();
+	virtual ~ClassTypeAccess();
+};
+
+} /* namespace alinous */
+
+#endif /* VARIABLE_ACCESS_CLASSTYPEACCESS_H_ */
