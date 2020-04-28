12e776e3d3d59a1f516c0553253c27bf706a22d3
iizuka <iizuka@alinous.org>
2020-04-28 22:43:36 +0900

    test interface access

--- src_test/smartcontract_vm/variables/test_allocation_if.cpp
+++ src_test/smartcontract_vm/variables/test_allocation_if.cpp
@@ -34,4 +34,20 @@ TEST(TestAllocationIfGroup, case01){
 	CHECK(!result)
 }
 
+TEST(TestAllocationIfGroup, case02){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/variables/resources/newobj/case02/", projectFolder);
 
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int iresult = VmTestUtils::getIntMemberValue(obj, L"count");
+	CHECK(iresult == 10);
+}
