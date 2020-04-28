336aaa79537925b760e8d7a3741b5ebcc1820164
iizuka <iizuka@alinous.org>
2020-04-28 16:32:37 +0900

    loop counter

--- src_test/smartcontract_vm/stmt_ctrl/test_do_while_stmt.cpp
+++ src_test/smartcontract_vm/stmt_ctrl/test_do_while_stmt.cpp
@@ -95,3 +95,22 @@ TEST(TestDoWhileStmtGroup, case04_err){
 	CHECK(!result)
 }
 
+TEST(TestDoWhileStmtGroup, case05){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_ctrl/resources/dowhile/case05/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int iresult = VmTestUtils::getIntMemberValue(obj, L"result");
+
+	CHECK(iresult == 20)
+}
+
--- src_test/smartcontract_vm/stmt_ctrl/test_while_stmt.cpp
+++ src_test/smartcontract_vm/stmt_ctrl/test_while_stmt.cpp
@@ -95,4 +95,24 @@ TEST(TestWhileStmtGroup, case04_err){
 	CHECK(!result)
 }
 
+TEST(TestWhileStmtGroup, case05){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/stmt_ctrl/resources/while/case05/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int iresult = VmTestUtils::getIntMemberValue(obj, L"result");
+
+	CHECK(iresult == 20)
+}
+
+
 
