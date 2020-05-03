796b936559376e7e03b26cc40f4ada5bef38484e
iizuka <iizuka@alinous.org>
2020-05-03 21:31:37 +0900

    test CODE_WRONG_FUNC_CALL_CANT_CALL_NOSTATIC

--- src_test/smartcontract_vm/access_inst/test_static_method_access.cpp
+++ src_test/smartcontract_vm/access_inst/test_static_method_access.cpp
@@ -97,4 +97,17 @@ TEST(TestStaticMethodAccessGroup, case04_err){
 	CHECK(util.hasAnalyzeError(ValidationError::CODE_WRONG_FUNC_CALL_CANT_INCOMPATIBLE_THIS));
 }
 
+TEST(TestStaticMethodAccessGroup, case05_err){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/access_inst/resources/staticm/case05_err/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+
+	CHECK(util.hasAnalyzeError(ValidationError::CODE_WRONG_FUNC_CALL_CANT_CALL_NOSTATIC));
+}
+
 
