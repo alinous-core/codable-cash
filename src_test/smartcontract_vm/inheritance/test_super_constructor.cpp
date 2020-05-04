62c388c3e7068a894274175381cec283e4c73ce2
iizuka <iizuka@alinous.org>
2020-05-04 21:51:09 +0900

    tested normal cases of super constructor

--- src_test/smartcontract_vm/inheritance/test_super_constructor.cpp
+++ src_test/smartcontract_vm/inheritance/test_super_constructor.cpp
@@ -14,6 +14,7 @@
 #include "../VmTestUtils.h"
 #include "ext_binary/ExtClassObject.h"
 
+#include "instance_exception_class/ExceptionClassDeclare.h"
 using namespace alinous;
 
 TEST_GROUP(TestSuperConstructorGroup) {
@@ -38,4 +39,42 @@ TEST(TestSuperConstructorGroup, case01){
 	int param = VmTestUtils::getIntMemberValue(obj, L"count");
 
 	CHECK(param == 10);
+}
+
+TEST(TestSuperConstructorGroup, case02){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/inheritance/resources/superconst/case02/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
+	int param = VmTestUtils::getIntMemberValue(obj, L"count");
+
+	CHECK(param == 10);
+}
+
+TEST(TestSuperConstructorGroup, case03){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/inheritance/resources/superconst/case03/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtExceptionObject* exobj = util.vm->getUncaughtException(); __STP(exobj);
+	const UnicodeString* exname = exobj->getClassName();
+
+	CHECK(exname->equals(ExceptionClassDeclare::NAME));
 }
