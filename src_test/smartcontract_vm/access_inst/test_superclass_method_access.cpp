a23ee1173aca45bcaec2a2dd2f2890a9ea4c95ce
iizuka <iizuka@alinous.org>
2020-05-05 18:05:12 +0900

    error case

--- src_test/smartcontract_vm/access_inst/test_superclass_method_access.cpp
+++ src_test/smartcontract_vm/access_inst/test_superclass_method_access.cpp
@@ -46,5 +46,16 @@ TEST(TestSuperClassMethodAccessGroup, case01){
 	ExtClassObject* obj = util.getMainExtObject(); __STP(obj);
 	int iresult = VmTestUtils::getIntMemberValue(obj, L"count");
 	CHECK(iresult == 20);
+}
+
+TEST(TestSuperClassMethodAccessGroup, case02_err){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/access_inst/resources/superclass/case02_err/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
 
 }
