250a8b44d8c4d1719f5f06c15ed9d5556dd69b65
iizuka <iizuka@alinous.org>
2020-05-20 23:01:23 +0900

    CdbDataFactory

--- src_smartcontract_db/table_record/CdbDataFactory.cpp
+++ src_smartcontract_db/table_record/CdbDataFactory.cpp
@@ -11,6 +11,16 @@
 
 #include "filestore_block/IBlockObject.h"
 
+#include "engine/CdbException.h"
+
+#include "table_record_value/AbstractCdbValue.h"
+#include "table_record_value/CdbByteValue.h"
+#include "table_record_value/CdbShortValue.h"
+#include "table_record_value/CdbIntValue.h"
+#include "table_record_value/CdbLongValue.h"
+#include "table_record_value/CdbStringValue.h"
+
+
 namespace codablecash {
 
 CdbDataFactory::CdbDataFactory() {
@@ -22,7 +32,32 @@ CdbDataFactory::~CdbDataFactory() {
 }
 
 IBlockObject* CdbDataFactory::makeDataFromBinary(ByteBuffer* in) {
+	uint8_t type = in->get();
+	AbstractCdbValue* value = nullptr;
 
+	switch(type){
+	case AbstractCdbValue::TYPE_BYTE:
+		value = new CdbByteValue();
+		break;
+	case AbstractCdbValue::TYPE_SHORT:
+		value = new CdbShortValue();
+		break;
+	case AbstractCdbValue::TYPE_INT:
+		value = new CdbIntValue();
+		break;
+	case AbstractCdbValue::TYPE_LONG:
+		value = new CdbLongValue();
+		break;
+	case AbstractCdbValue::TYPE_STRING:
+		value = new CdbStringValue();
+		break;
+	default:
+		throw new CdbException(__FILE__, __LINE__);
+	}
+
+	value->fromBinary(in);
+
+	return value;
 }
 
 } /* namespace codablecash */
