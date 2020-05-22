bd5b2efa381c8d531158d25396915576eca2dd7e
iizuka <iizuka@alinous.org>
2020-05-22 18:03:34 +0900

    CdbKeyFactory

--- src_smartcontract_db/table_record/CdbKeyFactory.cpp
+++ src_smartcontract_db/table_record/CdbKeyFactory.cpp
@@ -7,7 +7,15 @@
 
 #include "table_record/CdbKeyFactory.h"
 
+#include "table_record_key/AbstractCdbKey.h"
+
 #include "base_io/ByteBuffer.h"
+
+#include "table_record_key/CdbByteKey.h"
+#include "table_record_key/CdbShortKey.h"
+#include "table_record_key/CdbIntKey.h"
+#include "table_record_key/CdbLongKey.h"
+#include "table_record_key/CdbStringKey.h"
 
 namespace codablecash {
 
@@ -20,8 +28,34 @@ CdbKeyFactory::~CdbKeyFactory() {
 }
 
 AbstractBtreeKey* CdbKeyFactory::fromBinary(uint32_t keyType, ByteBuffer* in) const {
+	AbstractCdbKey* retKey = nullptr;
 
-	return BtreeKeyFactory::fromBinary(keyType, in);
+	switch(keyType){
+	case AbstractCdbKey::TYPE_BYTE:
+		retKey = new CdbByteKey();
+		break;
+	case AbstractCdbKey::TYPE_SHORT:
+		retKey = new CdbShortKey();
+		break;
+	case AbstractCdbKey::TYPE_INT:
+		retKey = new CdbIntKey();
+		break;
+	case AbstractCdbKey::TYPE_LONG:
+		retKey = new CdbLongKey();
+		break;
+	case AbstractCdbKey::TYPE_STRING:
+		retKey = new CdbStringKey();
+		break;
+	default:
+		break;
+	}
+
+	if(retKey != nullptr){
+		retKey->fromBinary(in);
+		return retKey;
+	}
+
+	return BtreeKeyFactory::fromBinary(keyType, in);;
 }
 
 } /* namespace codablecash */
