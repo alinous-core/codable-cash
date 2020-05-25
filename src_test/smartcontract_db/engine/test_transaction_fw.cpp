103c7f0d7342e63273a63b58e152fa0ece4c8f3c
iizuka <iizuka@alinous.org>
2020-05-25 17:14:35 +0900

    test ROLLBACK

--- src_test/smartcontract_db/engine/test_transaction_fw.cpp
+++ src_test/smartcontract_db/engine/test_transaction_fw.cpp
@@ -44,5 +44,19 @@ TEST(TestTransactionFwGroup, case01){
 
 	result = util.createInstance();
 	CHECK(result)
-
 }
+
+TEST(TestTransactionFwGroup, case02){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_db/engine/resources/trx/case02/", projectFolder, this->env);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+}
+
