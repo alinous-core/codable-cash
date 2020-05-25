f81e651cba0c6fc9b6a4fdaa18c01b888c36f365
iizuka <iizuka@alinous.org>
2020-05-25 17:35:00 +0900

    error cases

--- src_test/smartcontract_db/engine/test_transaction_fw.cpp
+++ src_test/smartcontract_db/engine/test_transaction_fw.cpp
@@ -17,6 +17,8 @@
 #include "../../smartcontract_vm/VmTestUtils.h"
 #include "vm_trx/VmTransactionHandlerException.h"
 
+#include "transaction_exception/DatabaseExceptionClassDeclare.h"
+
 using namespace alinous;
 
 TEST_GROUP(TestTransactionFwGroup) {
@@ -58,5 +60,65 @@ TEST(TestTransactionFwGroup, case02){
 
 	result = util.createInstance();
 	CHECK(result)
+}
+
+TEST(TestTransactionFwGroup, case03_err){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_db/engine/resources/trx/case03_err/", projectFolder, this->env);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtExceptionObject* e = util.vm->getUncaughtException(); __STP(e);
+	CHECK(e != nullptr)
+
+	const UnicodeString* exname = e->getClassName();
+	CHECK(exname->equals(&DatabaseExceptionClassDeclare::NAME))
+}
+
+TEST(TestTransactionFwGroup, case04_err){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_db/engine/resources/trx/case04_err/", projectFolder, this->env);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtExceptionObject* e = util.vm->getUncaughtException(); __STP(e);
+	CHECK(e != nullptr)
+
+	const UnicodeString* exname = e->getClassName();
+	CHECK(exname->equals(&DatabaseExceptionClassDeclare::NAME))
+}
+
+TEST(TestTransactionFwGroup, case05_err){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_db/engine/resources/trx/case05_err/", projectFolder, this->env);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtExceptionObject* e = util.vm->getUncaughtException(); __STP(e);
+	CHECK(e != nullptr)
+
+	const UnicodeString* exname = e->getClassName();
+	CHECK(exname->equals(&DatabaseExceptionClassDeclare::NAME))
 }
 
