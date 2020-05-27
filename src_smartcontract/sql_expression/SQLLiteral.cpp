70c1f06736291229936d93b388629b71c8d0e64a
iizuka <iizuka@alinous.org>
2020-05-27 14:55:18 +0900

    SQLLiteral

--- src_smartcontract/sql_expression/SQLLiteral.cpp
+++ src_smartcontract/sql_expression/SQLLiteral.cpp
@@ -6,13 +6,22 @@
  */
 
 #include "sql_expression/SQLLiteral.h"
+
 #include "base/UnicodeString.h"
+#include "base/Long.h"
+
+#include "sc_analyze/AnalyzedType.h"
+
+#include "instance_ref/PrimitiveReference.h"
+
+#include "instance_string/VmStringInstance.h"
 
 namespace alinous {
 
 SQLLiteral::SQLLiteral() : AbstractSQLExpression(CodeElement::SQL_EXP_LITERAL) {
 	this->value = nullptr;
 	this->type = TYPE_STRING;
+	this->longv = 0;
 }
 
 SQLLiteral::~SQLLiteral() {
@@ -46,5 +55,38 @@ void SQLLiteral::fromBinary(ByteBuffer* in) {
 	this->value = getString(in);
 	this->type = in->get();
 }
+
+void SQLLiteral::preAnalyze(AnalyzeContext* actx) {
+}
+
+void SQLLiteral::analyzeTypeRef(AnalyzeContext* actx) {
+}
+
+void SQLLiteral::analyze(AnalyzeContext* actx) {
+	if(this->type == SQLLiteral::TYPE_NUMBER){
+		this->longv = Long::parseLong(this->value);
+	}
+}
+
+AnalyzedType SQLLiteral::getType(AnalyzeContext* actx) {
+	if(this->type == SQLLiteral::TYPE_NUMBER){
+		return AnalyzedType(AnalyzedType::TYPE_LONG);
+	}
+
+	return AnalyzedType(AnalyzedType::TYPE_STRING);
+}
+
+void SQLLiteral::init(VirtualMachine* vm) {
+}
+
+AbstractVmInstance* SQLLiteral::interpret(VirtualMachine* vm) {
+	if(this->type == SQLLiteral::TYPE_NUMBER){
+		return PrimitiveReference::createLongReference(vm, this->longv);
+	}
+
+	VmStringInstance* inst = new(vm) VmStringInstance(vm, this->value);
+	return inst;
+}
+
 
 } /* namespace alinous */
