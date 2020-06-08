5acbd3296c0622b6b3ec7aa305e2565d264dd885
iizuka <iizuka@alinous.org>
2020-06-08 13:25:30 +0900

    TestInsertGroup

--- src_test/smartcontract_db/table/test_insert.cpp
+++ src_test/smartcontract_db/table/test_insert.cpp
@@ -5,6 +5,22 @@
  *      Author: iizuka
  */
 
+#include "test_utils/t_macros.h"
 
+#include "base/StackRelease.h"
 
+using namespace alinous;
+//using namespace codablecash;
 
+TEST_GROUP(TestInsertGroup) {
+	TEST_SETUP(){
+		env->setup();
+	}
+	TEST_TEARDOWN(){
+		env->teardown();
+	}
+};
+
+TEST(TestInsertGroup, case01) {
+
+}
