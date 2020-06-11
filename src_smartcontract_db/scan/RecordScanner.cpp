8d90f3f403f60870e2f3ede6fc5a3ad7985f2976
iizuka <iizuka@alinous.org>
2020-06-11 16:02:52 +0900

    RecordScanner

---
+++ src_smartcontract_db/scan/RecordScanner.cpp
@@ -0,0 +1,21 @@
+/*
+ * RecordScanner.cpp
+ *
+ *  Created on: 2020/06/11
+ *      Author: iizuka
+ */
+
+#include "scan/RecordScanner.h"
+
+namespace codablecash {
+
+RecordScanner::RecordScanner() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RecordScanner::~RecordScanner() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/scan/RecordScanner.h
@@ -0,0 +1,21 @@
+/*
+ * RecordScanner.h
+ *
+ *  Created on: 2020/06/11
+ *      Author: iizuka
+ */
+
+#ifndef SCAN_RECORDSCANNER_H_
+#define SCAN_RECORDSCANNER_H_
+
+namespace codablecash {
+
+class RecordScanner {
+public:
+	RecordScanner();
+	virtual ~RecordScanner();
+};
+
+} /* namespace codablecash */
+
+#endif /* SCAN_RECORDSCANNER_H_ */
