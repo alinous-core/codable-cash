34da314c03ca07f7aefde62cd190a3683d41d1dd
iizuka <iizuka@alinous.org>
2020-06-13 13:08:45 +0900

    metadata

---
+++ src_smartcontract_db/transaction_scan_result/ScanResultMetadata.cpp
@@ -0,0 +1,21 @@
+/*
+ * ScanResultMetadata.cpp
+ *
+ *  Created on: 2020/06/13
+ *      Author: iizuka
+ */
+
+#include <transaction_scan_result/ScanResultMetadata.h>
+
+namespace codablecash {
+
+ScanResultMetadata::ScanResultMetadata() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ScanResultMetadata::~ScanResultMetadata() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/transaction_scan_result/ScanResultMetadata.h
@@ -0,0 +1,21 @@
+/*
+ * ScanResultMetadata.h
+ *
+ *  Created on: 2020/06/13
+ *      Author: iizuka
+ */
+
+#ifndef TRANSACTION_SCAN_RESULT_SCANRESULTMETADATA_H_
+#define TRANSACTION_SCAN_RESULT_SCANRESULTMETADATA_H_
+
+namespace codablecash {
+
+class ScanResultMetadata {
+public:
+	ScanResultMetadata();
+	virtual ~ScanResultMetadata();
+};
+
+} /* namespace codablecash */
+
+#endif /* TRANSACTION_SCAN_RESULT_SCANRESULTMETADATA_H_ */
