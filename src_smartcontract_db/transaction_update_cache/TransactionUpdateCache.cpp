00582bbbe269a55c9fc81fcd2e27f84be3627794
iizuka <iizuka@alinous.org>
2020-06-13 17:39:09 +0900

    transaction_update_cache

---
+++ src_smartcontract_db/transaction_update_cache/TransactionUpdateCache.cpp
@@ -0,0 +1,21 @@
+/*
+ * TransactionUpdateCache.cpp
+ *
+ *  Created on: 2020/06/13
+ *      Author: iizuka
+ */
+
+#include <transaction_update_cache/TransactionUpdateCache.h>
+
+namespace codablecash {
+
+TransactionUpdateCache::TransactionUpdateCache() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TransactionUpdateCache::~TransactionUpdateCache() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/transaction_update_cache/TransactionUpdateCache.h
@@ -0,0 +1,21 @@
+/*
+ * TransactionUpdateCache.h
+ *
+ *  Created on: 2020/06/13
+ *      Author: iizuka
+ */
+
+#ifndef TRANSACTION_UPDATE_CACHE_TRANSACTIONUPDATECACHE_H_
+#define TRANSACTION_UPDATE_CACHE_TRANSACTIONUPDATECACHE_H_
+
+namespace codablecash {
+
+class TransactionUpdateCache {
+public:
+	TransactionUpdateCache();
+	virtual ~TransactionUpdateCache();
+};
+
+} /* namespace codablecash */
+
+#endif /* TRANSACTION_UPDATE_CACHE_TRANSACTIONUPDATECACHE_H_ */
