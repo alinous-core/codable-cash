37d8f5c47442f71b9a407969758c7f57d1df27dc
iizuka <iizuka@alinous.org>
2020-06-22 11:48:54 +0900

    test workbench of dom variable

---
+++ src_test/smartcontract_db/variable/test_dom_base.cpp
@@ -0,0 +1,18 @@
+/*
+ * test_dom_base.cpp
+ *
+ *  Created on: 2020/06/22
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+using namespace alinous;
+
+TEST_GROUP(TestDomBaseGroup) {
+	TEST_SETUP(){
+	}
+	TEST_TEARDOWN(){
+	}
+};
+
