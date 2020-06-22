d20a64c288aed9aa88105b9620c7ddc083806d17
iizuka <iizuka@alinous.org>
2020-06-22 19:44:12 +0900

    TestVmHashGroup test workbench

---
+++ src_test/smartcontract_db/variable/test_vm_hash.cpp
@@ -0,0 +1,25 @@
+/*
+ * test_vm_hash.cpp
+ *
+ *  Created on: 2020/06/22
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "vm/VirtualMachine.h"
+
+#include "instance_dom/DomVariableInstance.h"
+
+using namespace alinous;
+
+TEST_GROUP(TestVmHashGroup) {
+	TEST_SETUP(){
+	}
+	TEST_TEARDOWN(){
+	}
+};
+
+TEST(TestVmHashGroup, case01){
+
+}
