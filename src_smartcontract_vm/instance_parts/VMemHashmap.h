951dbf782ef05d7ba9470afe0b73497b32a32424
iizuka <iizuka@alinous.org>
2020-06-23 18:03:06 +0900

    VMemHashmap new & delete operator

--- src_smartcontract_vm/instance_parts/VMemHashmap.h
+++ src_smartcontract_vm/instance_parts/VMemHashmap.h
@@ -11,6 +11,10 @@
 #include "VMemHashMapRawArray.h"
 #include "VMemHashMapKeySet.h"
 
+#include "vm/VirtualMachine.h"
+
+#include "memory/VmMemoryManager.h"
+
 namespace alinous {
 
 template <typename K, typename V>
@@ -22,6 +26,25 @@ public:
 	}
 	virtual ~VMemHashmap(){
 		delete this->hashMapKeySet;
+	}
+
+	void* operator new(size_t size, VirtualMachine* vm){
+		VmMemoryManager* mem = vm->getMemory();
+		uint64_t mallocSize = size + sizeof(VirtualMachine*);
+
+		void* p = mem->malloc(mallocSize);
+		VirtualMachine** vmp = (VirtualMachine**)p;
+		*vmp = vm;
+
+		return ((char*)p) + sizeof(VirtualMachine*);
+	}
+	void operator delete(void* p, size_t size){
+		void* ptr = ((char*)p) - sizeof(VirtualMachine*);
+
+		VirtualMachine** vm = (VirtualMachine**)ptr;
+		VmMemoryManager* mem = (*vm)->getMemory();
+
+		mem->free((char*)ptr);
 	}
 
 protected:
