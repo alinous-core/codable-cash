263eec9d490097ea159977a2e3c379140ee588cf
iizuka <iizuka@alinous.org>
2020-06-26 15:12:19 +0900

    added TestDomVariableGroup

---
+++ src_test/smartcontract_db/variable/test_dom_variable.cpp
@@ -0,0 +1,35 @@
+/*
+ * test_dom_variable.cpp
+ *
+ *  Created on: 2020/06/26
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "base/StackRelease.h"
+
+#include "../../smartcontract_vm/VmTestUtils.h"
+#include "vm/VirtualMachine.h"
+
+using namespace alinous;
+
+TEST_GROUP(TestDomVariableGroup) {
+	TEST_SETUP(){
+	}
+	TEST_TEARDOWN(){
+	}
+};
+
+TEST(TestDomVariableGroup, testLocalDec01){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_db/variable/resources/local/case01/", projectFolder, this->env);
+
+	bool result = util.loadAllFiles();
+	CHECK(result)
+
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	result = util.analyze();
+	CHECK(result)
+}
