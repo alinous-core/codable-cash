41e77ed4f2570f861011121c836db749864957a2
iizuka <iizuka@alinous.org>
2020-06-26 13:06:43 +0900

    dom variable testing with GC

--- src_test/smartcontract_db/variable/test_dom_base.cpp
+++ src_test/smartcontract_db/variable/test_dom_base.cpp
@@ -11,6 +11,11 @@
 
 #include "instance_dom/DomVariableInstance.h"
 
+#include "instance_ref/VmRootReference.h"
+#include "instance_ref/AbstractReference.h"
+
+#include "instance_gc/GcManager.h"
+
 using namespace alinous;
 
 TEST_GROUP(TestDomBaseGroup) {
@@ -26,4 +31,20 @@ TEST(TestDomBaseGroup, case01){
 	DomVariableInstance* dom = new(&vm) DomVariableInstance(&vm);
 
 	delete dom;
+}
+
+TEST(TestDomBaseGroup, case02){
+	VirtualMachine* vm = new VirtualMachine(1024 * 10); __STP(vm);
+	GcManager* gc = vm->getGc();
+
+	{
+		VmRootReference* root = new(vm) VmRootReference(vm); __STP(root);
+
+		DomVariableInstance* dom = new(vm) DomVariableInstance(vm);
+		AbstractReference* ref = dom->wrap(root, vm); __STP(ref);
+
+		gc->removeObject(ref);
+
+		gc->garbageCollect();
+	}
 }
