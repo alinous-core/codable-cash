5ddc7636cdb90df01c184d9ea4720187cc5bbe7f
iizuka <iizuka@alinous.org>
2020-07-14 18:11:12 +0900

    error case

--- src_test/smartcontract_db/variable/test_dom_array.cpp
+++ src_test/smartcontract_db/variable/test_dom_array.cpp
@@ -20,6 +20,7 @@
 #include "ext_binary/ExtDomArrayObject.h"
 #include "ext_binary/ExtPrimitiveObject.h"
 
+#include "instance_exception/NullPointerExceptionClassDeclare.h"
 using namespace alinous;
 
 TEST_GROUP(TestDomArrayGroup) {
@@ -111,4 +112,26 @@ TEST(TestDomArrayGroup, case03){
 	int n = count->getIntValue();
 	CHECK(n == 5);
 
+}
+
+TEST(TestDomArrayGroup, case04_err){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_db/variable/resources/local_array/case04_err/", projectFolder, this->env);
+
+	bool result = util.loadAllFiles();
+	CHECK(result)
+
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	result = util.analyze();
+	CHECK(result)
+
+	result = util.createInstance();
+	CHECK(result)
+
+	ExtExceptionObject* ex = util.vm->getUncaughtException(); __STP(ex);
+	CHECK(ex != nullptr);
+
+	const UnicodeString* name = ex->getClassName();
+	CHECK(name->equals(NullPointerExceptionClassDeclare::NAME));
 }
