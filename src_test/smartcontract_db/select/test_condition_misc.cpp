eebad0ca9c4f92776d6ef1d38ceacb202d4ce18e
iizuka <iizuka@alinous.org>
2020-07-24 20:22:02 +0900

    added misc test

---
+++ src_test/smartcontract_db/select/test_condition_misc.cpp
@@ -0,0 +1,23 @@
+/*
+ * test_condition_misc.cpp
+ *
+ *  Created on: 2020/07/22
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "base/StackRelease.h"
+
+using namespace alinous;
+
+TEST_GROUP(TestConditionMiscGroup) {
+	TEST_SETUP(){
+	}
+	TEST_TEARDOWN(){
+	}
+};
+
+TEST(TestConditionMiscGroup, eq01){
+
+}
