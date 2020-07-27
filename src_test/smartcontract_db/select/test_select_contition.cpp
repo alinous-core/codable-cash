4680b831beb77645770ea1da9af6dcd5dc59fb99
iizuka <iizuka@alinous.org>
2020-07-27 19:44:55 +0900

    test numeric

--- src_test/smartcontract_db/select/test_select_contition.cpp
+++ src_test/smartcontract_db/select/test_select_contition.cpp
@@ -59,3 +59,31 @@ TEST(TestSelectConditionGroup, case01){
 		where->interpret(vm);
 	}
 }
+
+TEST(TestSelectConditionGroup, case02){
+	VirtualMachine* vm = new VirtualMachine(1024 * 10); __STP(vm);
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/select/resources/conditions/grp01/where02.alns"))
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		SQLWhere* where = lang->sqlWhere(); __STP(where);
+		CHECK(!parser.hasError())
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		where->preAnalyze(actx);
+		where->analyzeTypeRef(actx);
+		where->analyze(actx);
+
+		SelectScanPlanner* planner = new SelectScanPlanner(); __STP(planner);
+		VmSelectPlannerSetter setter(vm, planner);
+
+		where->init(vm);
+
+		where->interpret(vm);
+	}
+}
