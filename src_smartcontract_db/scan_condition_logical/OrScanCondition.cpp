40061d767c92bc818752bb853ae29311ac024bee
iizuka <iizuka@alinous.org>
2020-08-03 18:33:39 +0900

    OrScanCondition::toStringCode()

--- src_smartcontract_db/scan_condition_logical/OrScanCondition.cpp
+++ src_smartcontract_db/scan_condition_logical/OrScanCondition.cpp
@@ -15,15 +15,45 @@ using namespace alinous;
 namespace codablecash {
 
 OrScanCondition::OrScanCondition() : AbstractScanCondition(CodeElement::SQL_EXP_OR) {
-
+	this->str = nullptr;
 }
 
 OrScanCondition::~OrScanCondition() {
 	this->list.deleteElements();
+	resetStr();
 }
 
 void OrScanCondition::addCondition(AbstractScanCondition* cond) {
 	this->list.addElement(cond);
+	resetStr();
+}
+
+const UnicodeString* OrScanCondition::toStringCode() noexcept {
+	if(this->str == nullptr){
+		resetStr();
+
+		this->str = new UnicodeString(L"");
+
+		int maxLoop = this->list.size();
+		for(int i = 0; i != maxLoop; ++i){
+			AbstractScanCondition* cond = this->list.get(i);
+
+			if(i != 0){
+				this->str->append(L" OR ");
+			}
+
+			this->str->append(cond->toStringCode());
+		}
+	}
+
+	return this->str;
+}
+
+void OrScanCondition::resetStr() noexcept {
+	if(this->str != nullptr){
+		delete this->str;
+		this->str = nullptr;
+	}
 }
 
 } /* namespace codablecash */
