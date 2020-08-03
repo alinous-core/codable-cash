d1e80eff8c1e22dde1e9fcecf35aba3ecf3f64c2
iizuka <iizuka@alinous.org>
2020-08-03 22:44:15 +0900

    test relational conditions

--- src_test/smartcontract_db/select/test_select_contition.cpp
+++ src_test/smartcontract_db/select/test_select_contition.cpp
@@ -177,6 +177,114 @@ TEST(TestSelectConditionGroup, case04){
 	}
 }
 
+TEST(TestSelectConditionGroup, case05){
+	VirtualMachine* vm = new VirtualMachine(1024 * 10); __STP(vm);
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/select/resources/conditions/grp01/where05.alns"))
+
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		SQLWhere* where = lang->sqlWhere(); __STP(where);
+		CHECK(!parser.hasError())
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		where->preAnalyze(actx);
+		where->analyzeTypeRef(actx);
+		where->analyze(actx);
+
+		SelectScanPlanner* planner = new SelectScanPlanner(); __STP(planner);
+		VmSelectPlannerSetter setter(vm, planner);
+
+		where->init(vm);
+
+		where->interpret(vm);
+
+		ConditionsHolder* holder = planner->getConditions();
+		RootScanCondition* root = holder->getRoot();
+		const UnicodeString* str = root->toStringCode();
+
+		UnicodeString sql(L"id <= 10");
+		CHECK(sql.equals(str));
+	}
+}
+
+TEST(TestSelectConditionGroup, case06){
+	VirtualMachine* vm = new VirtualMachine(1024 * 10); __STP(vm);
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/select/resources/conditions/grp01/where06.alns"))
+
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		SQLWhere* where = lang->sqlWhere(); __STP(where);
+		CHECK(!parser.hasError())
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		where->preAnalyze(actx);
+		where->analyzeTypeRef(actx);
+		where->analyze(actx);
+
+		SelectScanPlanner* planner = new SelectScanPlanner(); __STP(planner);
+		VmSelectPlannerSetter setter(vm, planner);
+
+		where->init(vm);
+
+		where->interpret(vm);
+
+		ConditionsHolder* holder = planner->getConditions();
+		RootScanCondition* root = holder->getRoot();
+		const UnicodeString* str = root->toStringCode();
+
+		UnicodeString sql(L"id > 10");
+		CHECK(sql.equals(str));
+	}
+}
+
+TEST(TestSelectConditionGroup, case07){
+	VirtualMachine* vm = new VirtualMachine(1024 * 10); __STP(vm);
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/select/resources/conditions/grp01/where07.alns"))
+
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		SQLWhere* where = lang->sqlWhere(); __STP(where);
+		CHECK(!parser.hasError())
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		where->preAnalyze(actx);
+		where->analyzeTypeRef(actx);
+		where->analyze(actx);
+
+		SelectScanPlanner* planner = new SelectScanPlanner(); __STP(planner);
+		VmSelectPlannerSetter setter(vm, planner);
+
+		where->init(vm);
+
+		where->interpret(vm);
+
+		ConditionsHolder* holder = planner->getConditions();
+		RootScanCondition* root = holder->getRoot();
+		const UnicodeString* str = root->toStringCode();
+
+		UnicodeString sql(L"id < 10");
+		CHECK(sql.equals(str));
+	}
+}
+
 TEST(TestSelectConditionGroup, case05_not){
 	VirtualMachine* vm = new VirtualMachine(1024 * 10); __STP(vm);
 
