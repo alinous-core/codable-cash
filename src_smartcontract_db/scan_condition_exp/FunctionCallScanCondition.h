cd7c56e97ed9ea986274d5a36360f2e1bee9ad58
iizuka <iizuka@alinous.org>
2020-08-05 21:18:18 +0900

    FunctionCallScanCondition

---
+++ src_smartcontract_db/scan_condition_exp/FunctionCallScanCondition.h
@@ -0,0 +1,41 @@
+/*
+ * FunctionCallScanCondition.h
+ *
+ *  Created on: 2020/08/05
+ *      Author: iizuka
+ */
+
+#ifndef SCAN_CONDITION_EXP_FUNCTIONCALLSCANCONDITION_H_
+#define SCAN_CONDITION_EXP_FUNCTIONCALLSCANCONDITION_H_
+
+#include "scan_condition/AbstractScanCondition.h"
+
+#include "base/ArrayList.h"
+
+namespace codablecash {
+
+class IValueProvider;
+
+class FunctionCallScanCondition : public AbstractScanCondition {
+public:
+	FunctionCallScanCondition();
+	virtual ~FunctionCallScanCondition();
+
+	virtual const UnicodeString* toStringCode() noexcept;
+
+	void setName(UnicodeString* name) noexcept;
+	void addArgument(IValueProvider* arg) noexcept;
+
+private:
+	void resetStr() noexcept;
+
+private:
+	UnicodeString* name;
+	ArrayList<IValueProvider> arguments;
+
+	UnicodeString* str;
+};
+
+} /* namespace codablecash */
+
+#endif /* SCAN_CONDITION_EXP_FUNCTIONCALLSCANCONDITION_H_ */
