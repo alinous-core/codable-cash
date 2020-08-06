e81d74f3002d868ac3f1e02b8845595e50718b69
iizuka <iizuka@alinous.org>
2020-08-06 22:13:52 +0900

    tested ValidationError::CODE_WRONG_IMPORT_FORMAT

--- src_test/smartcontract_vm/inheritance/test_inheritance.cpp
+++ src_test/smartcontract_vm/inheritance/test_inheritance.cpp
@@ -33,7 +33,7 @@ TEST(TestInheritanceGroup, inheritance){
 
 }
 
-TEST(TestInheritanceGroup, inheritance_err){
+TEST(TestInheritanceGroup, inheritance_err01){
 	const File* projectFolder = this->env->getProjectRoot();
 	VmTestUtils util(L"src_test/smartcontract_vm/inheritance/resources/case02_err/", projectFolder);
 
@@ -44,4 +44,17 @@ TEST(TestInheritanceGroup, inheritance_err){
 	CHECK(!result)
 
 	CHECK(util.vm->hasAnalyzeError(ValidationError::CODE_WRONG_CLASS_NAME))
+}
+
+TEST(TestInheritanceGroup, inheritance_err02){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/inheritance/resources/case03_err/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(!result)
+
+	CHECK(util.vm->hasAnalyzeError(ValidationError::CODE_WRONG_IMPORT_FORMAT))
 }
