8b32c0dd3ae595234aa7a439ac398b3e7b915e9a
iizuka <iizuka@alinous.org>
2020-08-15 23:17:52 +0900

    testing workbench

--- src_test/smartcontract_db/table_scan/test_index_scanner.cpp
+++ src_test/smartcontract_db/table_scan/test_index_scanner.cpp
@@ -10,17 +10,25 @@
 #include "engine/CodableDatabase.h"
 
 #include "transaction/CdbTransaction.h"
-
 #include "transaction_log/CreateTableLog.h"
 
 #include "table/CdbTable.h"
+#include "table_record/CdbRecord.h"
 
 #include "table_record_value/AbstractCdbValue.h"
+
+#include "base/ArrayList.h"
+
+#include "transaction_log/InsertLog.h"
+
+#include "table_record/CdbTableIdentifier.h"
+#include "table_record_value/CdbIntValue.h"
+#include "table_record_value/CdbStringValue.h"
 
 using namespace codablecash;
 
 static void initDb(CodableDatabase& db, File* dbDir);
-
+static void insertRecord(CdbTransaction* trx, int id, const wchar_t* name, ArrayList<CdbRecord>* list);
 TEST_GROUP(TestIndexScannerGroup) {
 	TEST_SETUP(){
 		env->setup();
@@ -53,6 +61,28 @@ void initDb(CodableDatabase& db, File* dbDir) {
 	trx->commit();
 }
 
+void insertRecord(CdbTransaction* trx, int id, const wchar_t* name, ArrayList<CdbRecord>* list) {
+	InsertLog* log = new InsertLog();
+
+	CdbTableIdentifier* tableId = new CdbTableIdentifier();
+	tableId->setTable(new UnicodeString(L"test_table"));
+	log->setTable(tableId);
+
+	CdbRecord* record = new CdbRecord();
+	record->addValue(new CdbIntValue(1));
+
+	record->addValue(new CdbStringValue(name));
+
+	log->addRecord(record);
+
+	if(list != nullptr){
+		list->addElement((CdbRecord*)record->copy());
+	}
+
+
+	trx->insert(log);
+}
+
 TEST(TestIndexScannerGroup, case01){
 	File testCaseFolder = this->env->testCaseDir();
 	File* dbDir = testCaseFolder.get(L"db"); __STP(dbDir);
@@ -60,7 +90,15 @@ TEST(TestIndexScannerGroup, case01){
 
 	initDb(db, dbDir);
 
+	ArrayList<CdbRecord> list; list.setDeleteOnExit();
+	{
+		CdbTransaction* trx = db.newTransaction(); __STP(trx);
+		insertRecord(trx, 1, L"tanaka", &list);
+		insertRecord(trx, 2, L"yamada", &list);
+		insertRecord(trx, 3, L"yamamoto", &list);
 
+		trx->commit();
+	}
 
 	// FIXME TestIndexScannerGroup
 }
