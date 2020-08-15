ccd55a22c550eb9a1dd686295cfa27e24a0395dd
iizuka <iizuka@alinous.org>
2020-08-15 12:35:13 +0900

    more test of Btree

--- src_test/db/db_btree/test_btree.cpp
+++ src_test/db/db_btree/test_btree.cpp
@@ -241,11 +241,17 @@ TEST(TestBTreeGroup, add01){
 		int i = 3;
 		while(scanner->hasNext()){
 			const IBlockObject* obj = scanner->next();
+			const AbstractBtreeKey* k = scanner->nextKey();
+
 			const TempValue* tmp = dynamic_cast<const TempValue*>(obj);
 			uint64_t v = tmp->getValue();
 
+			const ULongKey* lk = dynamic_cast<const ULongKey*>(k);
+			uint64_t kv = lk->getValue();
+
 			uint64_t a = answers.get(i++);
 			CHECK(v == a)
+			CHECK(kv == a)
 		}
 	}
 	{
@@ -258,11 +264,17 @@ TEST(TestBTreeGroup, add01){
 		int i = 2;
 		while(scanner->hasNext()){
 			const IBlockObject* obj = scanner->next();
+			const AbstractBtreeKey* k = scanner->nextKey();
+
 			const TempValue* tmp = dynamic_cast<const TempValue*>(obj);
 			uint64_t v = tmp->getValue();
 
+			const ULongKey* lk = dynamic_cast<const ULongKey*>(k);
+			uint64_t kv = lk->getValue();
+
 			uint64_t a = answers.get(i++);
 			CHECK(v == a)
+			CHECK(kv == a)
 		}
 	}
 	{
@@ -360,11 +372,17 @@ TEST(TestBTreeGroup, add02){
 		int i = 0;
 		while(scanner->hasNext()){
 			const IBlockObject* obj = scanner->next();
+			const AbstractBtreeKey* k = scanner->nextKey();
+
 			const TempValue* tmp = dynamic_cast<const TempValue*>(obj);
 			uint64_t v = tmp->getValue();
 
+			const ULongKey* lk = dynamic_cast<const ULongKey*>(k);
+			uint64_t kv = lk->getValue();
+
 			uint64_t a = answers.get(i++);
 			CHECK(v == a)
+			CHECK(kv == a)
 		}
 	}
 
