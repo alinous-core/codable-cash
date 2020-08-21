61b1605b797787c760352b83719b537f9210ea4a
iizuka <iizuka@alinous.org>
2020-08-21 21:54:59 +0900

    FunctionCallScanColumnTarget::toStringCode()

--- src_smartcontract_db/scan_columns_exp/FunctionCallScanColumnTarget.cpp
+++ src_smartcontract_db/scan_columns_exp/FunctionCallScanColumnTarget.cpp
@@ -30,9 +30,24 @@ void FunctionCallScanColumnTarget::addArgument(AbstractScanColumnsTarget* arg) n
 }
 
 const UnicodeString* FunctionCallScanColumnTarget::toStringCode() noexcept {
-	// FIXME toStringCode()
 	if(this->str == nullptr){
 		this->str = new UnicodeString(L"");
+
+		this->str->append(this->name);
+
+		this->str->append(L"(");
+
+		int maxLoop = this->arguments.size();
+		for(int i = 0; i != maxLoop; ++i){
+			if(i > 0){
+				this->str->append(L", ");
+			}
+
+			AbstractScanColumnsTarget* value = this->arguments.get(i);
+			this->str->append(value->toStringCode());
+		}
+
+		this->str->append(L")");
 	}
 
 	return this->str;
