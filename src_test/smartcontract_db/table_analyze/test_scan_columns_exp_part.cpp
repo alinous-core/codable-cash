a4c47a1f42eaea8ee4be04509bd4f503ca607fe0
iizuka <iizuka@alinous.org>
2020-08-22 20:57:48 +0900

    test placeholder

--- src_test/smartcontract_db/table_analyze/test_scan_columns_exp_part.cpp
+++ src_test/smartcontract_db/table_analyze/test_scan_columns_exp_part.cpp
@@ -217,5 +217,43 @@ TEST(TestScanColumnsExpPartGroup, like01){
 		CHECK(str->equals(ans));
 	}
 }
+
+TEST(TestScanColumnsExpPartGroup, placeholder01){
+	VirtualMachine* vm = new VirtualMachine(1024 * 10); __STP(vm);
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/table_analyze/resources/columns_exp/select09.alns"))
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		SelectStatement* stmt = lang->selectStatement(); __STP(stmt);
+		CHECK(!parser.hasError())
+
+		SQLSelectTargetList* selectList = stmt->getSQLSelectTargetList();
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		selectList->preAnalyze(actx);
+		selectList->analyzeTypeRef(actx);
+		//selectList->analyze(actx);
+
+
+		SelectScanPlanner* planner = new SelectScanPlanner(); __STP(planner);
+		VmSelectPlannerSetter setter(vm, planner);
+
+		selectList->init(vm);
+		selectList->interpret(vm);
+
+		ScanColumnHolder* holder = planner->getColumnHolder();
+		UnicodeString* str = holder->toCodeString(); __STP(str);
+
+		UnicodeString ans(L"func01(${})");
+		CHECK(str->equals(ans));
+	}
+}
+
+
 // FIXME testing now
 
