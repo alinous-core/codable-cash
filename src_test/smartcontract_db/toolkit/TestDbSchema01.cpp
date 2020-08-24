b6514498b9b4c502023203a6fc690e24c6fa834d
iizuka <iizuka@alinous.org>
2020-08-24 18:28:47 +0900

    DB testing toolkit folder

---
+++ src_test/smartcontract_db/toolkit/TestDbSchema01.cpp
@@ -0,0 +1,21 @@
+/*
+ * TestDbSchema01.cpp
+ *
+ *  Created on: 2020/08/24
+ *      Author: iizuka
+ */
+
+#include "TestDbSchema01.h"
+
+namespace codablecash {
+
+TestDbSchema01::TestDbSchema01() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TestDbSchema01::~TestDbSchema01() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_test/smartcontract_db/toolkit/TestDbSchema01.h
@@ -0,0 +1,21 @@
+/*
+ * TestDbSchema01.h
+ *
+ *  Created on: 2020/08/24
+ *      Author: iizuka
+ */
+
+#ifndef SMARTCONTRACT_DB_TOOLKIT_TESTDBSCHEMA01_H_
+#define SMARTCONTRACT_DB_TOOLKIT_TESTDBSCHEMA01_H_
+
+namespace codablecash {
+
+class TestDbSchema01 {
+public:
+	TestDbSchema01();
+	virtual ~TestDbSchema01();
+};
+
+} /* namespace codablecash */
+
+#endif /* SMARTCONTRACT_DB_TOOLKIT_TESTDBSCHEMA01_H_ */
