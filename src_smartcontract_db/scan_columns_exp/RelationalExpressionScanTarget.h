98a893da06491f0b38de703111587a05ee535993
iizuka <iizuka@alinous.org>
2020-08-28 11:51:04 +0900

    RelationalExpressionScanTarget::resolveColumns

--- src_smartcontract_db/scan_columns_exp/RelationalExpressionScanTarget.h
+++ src_smartcontract_db/scan_columns_exp/RelationalExpressionScanTarget.h
@@ -24,6 +24,8 @@ public:
 
 	virtual const UnicodeString* toStringCode() noexcept;
 
+	virtual void resolveColumns(VirtualMachine* vm, SelectScanPlanner* planner);
+
 private:
 	AbstractScanColumnsTarget* left;
 	AbstractScanColumnsTarget* right;
