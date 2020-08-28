85f156636728aa6d172f8070d959827cda6ca367
iizuka <iizuka@alinous.org>
2020-08-28 20:11:20 +0900

    AbstractScannerFactory

---
+++ src_smartcontract_db/scan_planner_scanner/AbstractScannerFactory.cpp
@@ -0,0 +1,21 @@
+/*
+ * AbstractScannerFactory.cpp
+ *
+ *  Created on: 2020/08/28
+ *      Author: iizuka
+ */
+
+#include <scan_planner_scanner/AbstractScannerFactory.h>
+
+namespace codablecash {
+
+AbstractScannerFactory::AbstractScannerFactory() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AbstractScannerFactory::~AbstractScannerFactory() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/scan_planner_scanner/AbstractScannerFactory.h
@@ -0,0 +1,21 @@
+/*
+ * AbstractScannerFactory.h
+ *
+ *  Created on: 2020/08/28
+ *      Author: iizuka
+ */
+
+#ifndef SCAN_PLANNER_SCANNER_ABSTRACTSCANNERFACTORY_H_
+#define SCAN_PLANNER_SCANNER_ABSTRACTSCANNERFACTORY_H_
+
+namespace codablecash {
+
+class AbstractScannerFactory {
+public:
+	AbstractScannerFactory();
+	virtual ~AbstractScannerFactory();
+};
+
+} /* namespace codablecash */
+
+#endif /* SCAN_PLANNER_SCANNER_ABSTRACTSCANNERFACTORY_H_ */
