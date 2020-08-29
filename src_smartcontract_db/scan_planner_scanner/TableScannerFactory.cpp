a135d8ff84a5e410e326742178dfd8c428560651
iizuka <iizuka@alinous.org>
2020-08-29 12:36:55 +0900

    TableScannerFactory

---
+++ src_smartcontract_db/scan_planner_scanner/TableScannerFactory.cpp
@@ -0,0 +1,20 @@
+/*
+ * TableScannerFactory.cpp
+ *
+ *  Created on: 2020/08/29
+ *      Author: iizuka
+ */
+
+#include "scan_planner_scanner/TableScannerFactory.h"
+
+namespace codablecash {
+
+TableScannerFactory::TableScannerFactory() {
+
+}
+
+TableScannerFactory::~TableScannerFactory() {
+
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/scan_planner_scanner/TableScannerFactory.h
@@ -0,0 +1,21 @@
+/*
+ * TableScannerFactory.h
+ *
+ *  Created on: 2020/08/29
+ *      Author: iizuka
+ */
+
+#ifndef SCAN_PLANNER_SCANNER_TABLESCANNERFACTORY_H_
+#define SCAN_PLANNER_SCANNER_TABLESCANNERFACTORY_H_
+
+namespace codablecash {
+
+class TableScannerFactory {
+public:
+	TableScannerFactory();
+	virtual ~TableScannerFactory();
+};
+
+} /* namespace codablecash */
+
+#endif /* SCAN_PLANNER_SCANNER_TABLESCANNERFACTORY_H_ */
