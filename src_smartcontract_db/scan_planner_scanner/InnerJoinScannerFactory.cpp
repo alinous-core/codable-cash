48b625363ea4f9e209680d2799ba7799dc943b0b
iizuka <iizuka@alinous.org>
2020-08-29 12:50:01 +0900

    InnerJoinScannerFactory

---
+++ src_smartcontract_db/scan_planner_scanner/InnerJoinScannerFactory.cpp
@@ -0,0 +1,21 @@
+/*
+ * InnerJoinFactory.cpp
+ *
+ *  Created on: 2020/08/29
+ *      Author: iizuka
+ */
+
+#include "scan_planner_scanner/InnerJoinScannerFactory.h"
+
+namespace codablecash {
+
+InnerJoinScannerFactory::InnerJoinScannerFactory() {
+	// TODO Auto-generated constructor stub
+
+}
+
+InnerJoinScannerFactory::~InnerJoinScannerFactory() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/scan_planner_scanner/InnerJoinScannerFactory.h
@@ -0,0 +1,21 @@
+/*
+ * InnerJoinFactory.h
+ *
+ *  Created on: 2020/08/29
+ *      Author: iizuka
+ */
+
+#ifndef SCAN_PLANNER_SCANNER_INNERJOINSCANNERFACTORY_H_
+#define SCAN_PLANNER_SCANNER_INNERJOINSCANNERFACTORY_H_
+
+namespace codablecash {
+
+class InnerJoinScannerFactory {
+public:
+	InnerJoinScannerFactory();
+	virtual ~InnerJoinScannerFactory();
+};
+
+} /* namespace codablecash */
+
+#endif /* SCAN_PLANNER_SCANNER_INNERJOINSCANNERFACTORY_H_ */
