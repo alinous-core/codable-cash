c84134b042eea4500333401031554e8d8b1de5ae
iizuka <iizuka@alinous.org>
2020-09-06 16:13:27 +0900

    ConditionsHolderStackMarker

---
+++ src_smartcontract_db/scan_planner/ConditionsHolderStackMarker.cpp
@@ -0,0 +1,23 @@
+/*
+ * ConditionsHolderStackRelease.cpp
+ *
+ *  Created on: 2020/09/06
+ *      Author: iizuka
+ */
+
+#include "scan_planner/ConditionsHolderStackMarker.h"
+
+#include "scan_planner/ConditionsHolderStack.h"
+
+namespace codablecash {
+
+ConditionsHolderStackMarker::ConditionsHolderStackMarker(ConditionsHolderStack* stack) {
+	this->stack = stack;
+	stack->mark();
+}
+
+ConditionsHolderStackMarker::~ConditionsHolderStackMarker() {
+	this->stack->unmark();
+}
+
+} /* namespace codablecash */
