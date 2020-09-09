9bc87cc3ecacaa0264f7332ae053a8273a0c0dec
iizuka <iizuka@alinous.org>
2020-09-09 20:08:58 +0900

    test bench of alter commands

---
+++ src_test/smartcontract_db/table_alter/test_alter_parse.cpp
@@ -0,0 +1,22 @@
+/*
+ * test_alter_parse.cpp
+ *
+ *  Created on: 2020/09/09
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "base/StackRelease.h"
+
+
+TEST_GROUP(TestAlterParseGroup) {
+	TEST_SETUP(){
+	}
+	TEST_TEARDOWN(){
+	}
+};
+
+TEST(TestAlterParseGroup, AlterAddColumnCommand01){
+
+}
