8897a36f8822948d1f16f0d7049de7a9756a143f
iizuka <iizuka@alinous.org>
2020-09-09 22:51:26 +0900

    AlterAddIndexCommand binary format

--- src_smartcontract/sql_ddl_alter/AlterAddIndexCommand.cpp
+++ src_smartcontract/sql_ddl_alter/AlterAddIndexCommand.cpp
@@ -29,15 +29,45 @@ void AlterAddIndexCommand::addColumn(UnicodeString* colName) noexcept {
 }
 
 int AlterAddIndexCommand::binarySize() const {
+	checkNotNull(this->name);
+
 	int total = sizeof(uint16_t);
+	total += stringSize(this->name);
+
+	int maxLoop = this->list.size();
+	total += sizeof(int32_t);
+
+	for(int i = 0; i != maxLoop; ++i){
+		UnicodeString* col = this->list.get(i);
+		total += stringSize(col);
+	}
 
 	return total;
 }
 
 void AlterAddIndexCommand::toBinary(ByteBuffer* out) {
+	checkNotNull(this->name);
+
+	out->putShort(CodeElement::DDL_ALTER_ADD_INDEX);
+	putString(out, this->name);
+
+	int maxLoop = this->list.size();
+	out->putInt(maxLoop);
+
+	for(int i = 0; i != maxLoop; ++i){
+		UnicodeString* col = this->list.get(i);
+		putString(out, col);
+	}
 }
 
 void AlterAddIndexCommand::fromBinary(ByteBuffer* in) {
+	this->name = getString(in);
+
+	int maxLoop = in->getInt();
+	for(int i = 0; i != maxLoop; ++i){
+		UnicodeString* col = getString(in);
+		addColumn(col);
+	}
 }
 
 } /* namespace alinous */
