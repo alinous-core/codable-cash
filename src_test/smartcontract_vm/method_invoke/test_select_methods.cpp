86c313c8addb40fefd4badac2b05440605171fb0
iizuka <iizuka@alinous.org>
2020-09-13 19:14:05 +0900

    test char

--- src_test/smartcontract_vm/method_invoke/test_select_methods.cpp
+++ src_test/smartcontract_vm/method_invoke/test_select_methods.cpp
@@ -73,3 +73,14 @@ TEST(TestSelectMethodGroup, case04){
 	CHECK(result)
 }
 
+TEST(TestSelectMethodGroup, case05){
+	const File* projectFolder = this->env->getProjectRoot();
+	VmTestUtils util(L"src_test/smartcontract_vm/method_invoke/resources/select/case05/", projectFolder);
+
+	util.loadAllFiles();
+	util.setMain(L"test.fw", L"SmartContract", L"main");
+
+	bool result = util.analyze();
+	CHECK(result)
+}
+
