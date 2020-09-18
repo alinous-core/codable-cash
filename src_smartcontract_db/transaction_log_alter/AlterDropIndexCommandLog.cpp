86865e54ea0dcb5ce1268f7df5d0c434d9870786
iizuka <iizuka@alinous.org>
2020-09-18 13:23:36 +0900

    AlterDropIndexCommandLog::commit

--- src_smartcontract_db/transaction_log_alter/AlterDropIndexCommandLog.cpp
+++ src_smartcontract_db/transaction_log_alter/AlterDropIndexCommandLog.cpp
@@ -9,6 +9,12 @@
 
 #include "sql_ddl_alter/AlterDropIndexCommand.h"
 
+#include "transaction/CdbTransactionManager.h"
+
+#include "engine_lock/WriteLockHandle.h"
+
+#include "base/StackRelease.h"
+
 namespace codablecash {
 
 AlterDropIndexCommandLog::AlterDropIndexCommandLog() : AbstractAlterCommandLog(AbstractTransactionLog::TRX_ALTER_DROP_INDEX) {
@@ -47,6 +53,9 @@ void AlterDropIndexCommandLog::fromBinary(ByteBuffer* in) {
 }
 
 void AlterDropIndexCommandLog::commit(CdbTransactionManager* trxManager) {
+	WriteLockHandle* lockH = trxManager->databaseWriteLock(); __STP(lockH);
+
+	trxManager->commitAlterTable(this);
 }
 
 } /* namespace codablecash */
