8a5011fed06e7674b3416247f97c9e942b4f4af9
iizuka <iizuka@alinous.org>
2020-09-23 14:14:58 +0900

    folder to make datase for alter test

---
+++ src_test/smartcontract_db/toolkit_alter/TestDbSchemaAlter01.cpp
@@ -0,0 +1,21 @@
+/*
+ * TestDbSchemaAlter01.cpp
+ *
+ *  Created on: 2020/09/23
+ *      Author: iizuka
+ */
+
+#include "TestDbSchemaAlter01.h"
+
+namespace codablecash {
+
+TestDbSchemaAlter01::TestDbSchemaAlter01() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TestDbSchemaAlter01::~TestDbSchemaAlter01() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_test/smartcontract_db/toolkit_alter/TestDbSchemaAlter01.h
@@ -0,0 +1,21 @@
+/*
+ * TestDbSchemaAlter01.h
+ *
+ *  Created on: 2020/09/23
+ *      Author: iizuka
+ */
+
+#ifndef SMARTCONTRACT_DB_TOOLKIT_ALTER_TESTDBSCHEMAALTER01_H_
+#define SMARTCONTRACT_DB_TOOLKIT_ALTER_TESTDBSCHEMAALTER01_H_
+
+namespace codablecash {
+
+class TestDbSchemaAlter01 {
+public:
+	TestDbSchemaAlter01();
+	virtual ~TestDbSchemaAlter01();
+};
+
+} /* namespace codablecash */
+
+#endif /* SMARTCONTRACT_DB_TOOLKIT_ALTER_TESTDBSCHEMAALTER01_H_ */
