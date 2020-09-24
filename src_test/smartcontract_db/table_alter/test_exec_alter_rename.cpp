3c7a78b83340babfe42ea05e3bcf5133eb0e0b79
iizuka <iizuka@alinous.org>
2020-09-24 17:45:39 +0900

    test bench for alter rename commands

---
+++ src_test/smartcontract_db/table_alter/test_exec_alter_rename.cpp
@@ -0,0 +1,100 @@
+/*
+ * test_exec_alter_rename.cpp
+ *
+ *  Created on: 2020/09/24
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "base/StackRelease.h"
+
+#include "vm/VirtualMachine.h"
+
+#include "compiler/SmartContractParser.h"
+
+#include "alinous_lang/AlinousLang.h"
+#include "../toolkit/TestDbSchema01.h"
+#include "../toolkit_alter/TestDbSchemaAlter01.h"
+#include "../toolkit_alter/TestDbSchemaAlter02.h"
+#include "../toolkit_alter/TestDbSchemaAlter03.h"
+
+#include "sc_analyze/AnalyzeContext.h"
+
+#include "engine/CodableDatabase.h"
+
+#include "schema/SchemaManager.h"
+
+#include "table/CdbTable.h"
+#include "table/CdbTableColumn.h"
+#include "table/CdbTableIndex.h"
+
+TEST_GROUP(TestExecAlterRenameGroup) {
+	TEST_SETUP() {
+		env->setup();
+	}
+	TEST_TEARDOWN() {
+		env->teardown();
+	}
+};
+
+/**
+ * ALTER TABLE test_table RENAME TO test_renamed;
+ */
+TEST(TestExecAlterRenameGroup, renameTable01){
+	TestDbSchemaAlter01 tester(this->env);
+	tester.init(1024*10);
+	tester.insert01();
+
+	VirtualMachine* vm = tester.getVm();
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/table_alter/resources/exec_rename/renameTable01.alns"))
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		AlterTableStatement* stmt = lang->alterTableStatement(); __STP(stmt);
+		CHECK(!parser.hasError())
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		stmt->preAnalyze(actx);
+		stmt->analyzeTypeRef(actx);
+		stmt->analyze(actx);
+
+		stmt->interpret(vm);
+	}
+}
+
+/**
+ * ALTER TABLE test_table RENAME id TO id2;
+ */
+TEST(TestExecAlterRenameGroup, renameColumn01){
+	TestDbSchemaAlter01 tester(this->env);
+	tester.init(1024*10);
+	tester.insert01();
+
+	VirtualMachine* vm = tester.getVm();
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/table_alter/resources/exec_rename/renameColumn01.alns"))
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		AlterTableStatement* stmt = lang->alterTableStatement(); __STP(stmt);
+		CHECK(!parser.hasError())
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		stmt->preAnalyze(actx);
+		stmt->analyzeTypeRef(actx);
+		stmt->analyze(actx);
+
+		stmt->interpret(vm);
+	}
+}
+
