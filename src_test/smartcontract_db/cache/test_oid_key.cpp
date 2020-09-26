6f28571ad74bded6f961012bae495bb53cfa5e5e
iizuka <iizuka@alinous.org>
2020-09-26 18:48:22 +0900

    binary functions

--- src_test/smartcontract_db/cache/test_oid_key.cpp
+++ src_test/smartcontract_db/cache/test_oid_key.cpp
@@ -12,11 +12,15 @@
 #include "engine/CdbOid.h"
 
 #include "table_record_key/CdbOidKey.h"
+#include "table_record/CdbKeyFactory.h"
 
 #include "btree/AbstractBtreeKey.h"
 
 #include "btreekey/NullKey.h"
 #include "btreekey/InfinityKey.h"
+
+#include "base_io/ByteBuffer.h"
+
 
 using namespace codablecash;
 
@@ -54,3 +58,42 @@ TEST(TestOidKeyGroup, case03){
 	int diff = key.compareTo(&infkey);
 	CHECK(diff < 0);
 }
+
+static bool checkBinary(ByteBuffer* buff);
+bool checkBinary(ByteBuffer* buff){
+	CdbKeyFactory factory;
+
+	int lastSize = buff->capacity();
+
+	buff->position(0);
+
+	int32_t keytype = buff->getInt();
+	AbstractBtreeKey* element = factory.fromBinary(keytype, buff);
+
+	int size = element->binarySize();
+	if(lastSize != size){
+		return false;
+	}
+
+	ByteBuffer* buff2 = ByteBuffer::allocateWithEndian(size, true); __STP(buff2);
+	element->toBinary(buff2);
+
+	if(buff2->position() != size){
+		return false;
+	}
+
+	return Mem::memcmp(buff->array(), buff2->array(), size) == 0;
+}
+
+TEST(TestOidKeyGroup, binary01){
+	CdbOid oid(1);
+	CdbOidKey key(&oid);
+
+	int size = key.binarySize();
+	ByteBuffer* buff = ByteBuffer::allocateWithEndian(size, true); __STP(buff);
+	key.toBinary(buff);
+
+	bool result = checkBinary(buff);
+	CHECK(result)
+}
+
