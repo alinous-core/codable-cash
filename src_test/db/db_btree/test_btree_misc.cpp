f38691b0ef786ac98904e603d234342143692dc1
iizuka <iizuka@alinous.org>
2020-09-27 19:03:17 +0900

    DataNode

--- src_test/db/db_btree/test_btree_misc.cpp
+++ src_test/db/db_btree/test_btree_misc.cpp
@@ -7,9 +7,15 @@
 
 
 #include "test_utils/t_macros.h"
-#include "btree/BtreeConfig.h"
 
 #include "base/StackRelease.h"
+
+#include "btree/BtreeHeaderBlock.h"
+#include "btree/BtreeConfig.h"
+#include "btree/AbstractBtreeKey.h"
+#include "btree/DataNode.h"
+
+#include "btreekey/NullKey.h"
 
 using namespace alinous;
 
@@ -27,4 +33,32 @@ TEST(TestBTreeMiscGroup, case01){
 
 	CHECK(config.blockSize = config2->blockSize);
 	CHECK(config.nodeNumber = config2->blockSize);
+}
+
+TEST(TestBTreeMiscGroup, case02){
+	BtreeHeaderBlock block;
+
+	BtreeHeaderBlock* block2 = dynamic_cast<BtreeHeaderBlock*>(block.copyData()); __STP(block2);
+
+	CHECK(block.getRootFpos() == block2->getRootFpos());
+}
+
+TEST(TestBTreeMiscGroup, case03){
+	AbstractBtreeKey* key = new NullKey(); __STP(key);
+
+	AbstractBtreeKey* key2 = dynamic_cast<AbstractBtreeKey*>(key->copyData()); __STP(key2);
+
+	CHECK(key->compareTo(key2) == 0);
+}
+
+TEST(TestBTreeMiscGroup, case04){
+	AbstractBtreeKey* key = new NullKey();
+	DataNode node(key);
+	node.setDataFpos(10L);
+	node.setFpos(1L);
+
+	DataNode* node2 = dynamic_cast<DataNode*>(node.copyData()); __STP(node2);
+
+	CHECK(node.getFpos() == node2->getFpos());
+	CHECK(node.getDataFpos() == node2->getDataFpos());
 }
