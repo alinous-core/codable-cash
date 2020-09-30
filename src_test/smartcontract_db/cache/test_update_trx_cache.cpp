9e0ed69de0c753fcb4018daeb47bb1d66800ef9d
iizuka <iizuka@alinous.org>
2020-09-30 17:39:53 +0900

    makeRecord() for test

--- src_test/smartcontract_db/cache/test_update_trx_cache.cpp
+++ src_test/smartcontract_db/cache/test_update_trx_cache.cpp
@@ -22,6 +22,9 @@
 
 #include "../toolkit/TestDbSchema01.h"
 
+#include "table_record/CdbRecord.h"
+#include "table_record_value/CdbIntValue.h"
+
 using namespace alinous;
 using namespace codablecash;
 
@@ -33,6 +36,18 @@ TEST_GROUP(TestUpdateTrxCacheGroup) {
 		env->teardown();
 	}
 };
+
+static CdbRecord* makeRecord(uint64_t loid, int value);
+CdbRecord* makeRecord(uint64_t loid, int value){
+	CdbRecord* record = new CdbRecord();
+	CdbOid oid(loid);
+	record->setOid(&oid);
+
+	CdbIntValue* cvalue = new CdbIntValue(value);
+	record->addValue(cvalue);
+
+	return record;
+}
 
 TEST(TestUpdateTrxCacheGroup, case01){
 	TestDbSchema01 tester(this->env);
