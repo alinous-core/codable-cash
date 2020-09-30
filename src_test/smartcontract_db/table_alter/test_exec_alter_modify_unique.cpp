59c47fff9b996e6b227b219e3935e4a42a025bec
iizuka <iizuka@alinous.org>
2020-10-01 00:02:57 +0900

    unique test files

---
+++ src_test/smartcontract_db/table_alter/test_exec_alter_modify_unique.cpp
@@ -0,0 +1,19 @@
+/*
+ * test_exec_alter_modify_unique.cpp
+ *
+ *  Created on: 2020/09/30
+ *      Author: iizuka
+ */
+
+
+
+
+
+/**
+ * set primary key unique
+ */
+
+
+/**
+ * set primary key not unique
+ */
