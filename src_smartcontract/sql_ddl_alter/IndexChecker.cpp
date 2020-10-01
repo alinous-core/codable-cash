c0422d7fd663633facc79c1d65880aee7b0a745e
iizuka <iizuka@alinous.org>
2020-10-01 12:40:53 +0900

    added IndexChecker

---
+++ src_smartcontract/sql_ddl_alter/IndexChecker.cpp
@@ -0,0 +1,21 @@
+/*
+ * IndexChecker.cpp
+ *
+ *  Created on: 2020/10/01
+ *      Author: iizuka
+ */
+
+#include <sql_ddl_alter/IndexChecker.h>
+
+namespace codablecash {
+
+IndexChecker::IndexChecker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IndexChecker::~IndexChecker() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract/sql_ddl_alter/IndexChecker.h
@@ -0,0 +1,21 @@
+/*
+ * IndexChecker.h
+ *
+ *  Created on: 2020/10/01
+ *      Author: iizuka
+ */
+
+#ifndef SQL_DDL_ALTER_INDEXCHECKER_H_
+#define SQL_DDL_ALTER_INDEXCHECKER_H_
+
+namespace codablecash {
+
+class IndexChecker {
+public:
+	IndexChecker();
+	virtual ~IndexChecker();
+};
+
+} /* namespace codablecash */
+
+#endif /* SQL_DDL_ALTER_INDEXCHECKER_H_ */
