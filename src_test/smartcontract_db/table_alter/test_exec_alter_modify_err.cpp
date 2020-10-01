d608ea974e744337cce5b0fa55429a2437302e45
iizuka <iizuka@alinous.org>
2020-10-01 21:19:24 +0900

    Length error

--- src_test/smartcontract_db/table_alter/test_exec_alter_modify_err.cpp
+++ src_test/smartcontract_db/table_alter/test_exec_alter_modify_err.cpp
@@ -109,3 +109,34 @@ TEST(TestExecAlterMofdifyErrGroup, case02){
 	}
 }
 
+/**
+ * default value error
+ * ALTER TABLE test_table MODIFY email_id VARCHAR('test');
+ */
+TEST(TestExecAlterMofdifyErrGroup, case03){
+	TestDbSchemaAlter01 tester(this->env);
+	tester.init(1024*10);
+	tester.insert03();
+
+	VirtualMachine* vm = tester.getVm();
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/table_alter/resources/exec_alter_modify_err/case03.alns"))
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		AlterTableStatement* stmt = lang->alterTableStatement(); __STP(stmt);
+		CHECK(!parser.hasError())
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		stmt->preAnalyze(actx);
+		stmt->analyzeTypeRef(actx);
+		stmt->analyze(actx);
+
+		vm->hasAnalyzeError(ValidationError::DB_LENGTH_IS_NOT_CORRECT_INTEGER);
+	}
+}
+
