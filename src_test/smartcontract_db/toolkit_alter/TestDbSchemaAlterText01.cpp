932bd44ff418f7d5d67cf329301c1e01c0412c9e
iizuka <iizuka@alinous.org>
2020-10-02 19:40:57 +0900

    TestDbSchemaAlterText01 test bench

--- src_test/smartcontract_db/toolkit_alter/TestDbSchemaAlterText01.cpp
+++ src_test/smartcontract_db/toolkit_alter/TestDbSchemaAlterText01.cpp
@@ -43,15 +43,60 @@ TestDbSchemaAlterText01::~TestDbSchemaAlterText01() {
 }
 
 void TestDbSchemaAlterText01::init(uint64_t memCapacity) {
+	TestDbSchemaBase::init(memCapacity);
+	createTable();
 }
 
 void TestDbSchemaAlterText01::createTable() {
+	CdbTransaction* trx = getDatabase()->newTransaction(); __STP(trx);
+
+	CreateTableLog* cmd = new CreateTableLog();
+	CdbTable* table = new CdbTable(0);
+
+	UnicodeString* testSchema = new UnicodeString(L"public");
+	table->setSchemaName(testSchema);
+	table->setName(new UnicodeString(L"test_table"));
+
+	table->addColumn(0, L"id", AbstractCdbValue::TYPE_INT, 0, true, true, nullptr);
+	table->addColumn(0, L"name", AbstractCdbValue::TYPE_STRING, 8, false, false, L"");
+	table->addColumn(0, L"email_id", AbstractCdbValue::TYPE_INT, 0, false, true, L"0");
+
+	table->setPrimaryKey(L"id");
+
+	cmd->setTable(table);
+
+	trx->createTable(cmd);
+	trx->commit();
 }
 
 void TestDbSchemaAlterText01::insert01() {
+	CdbTransaction* trx = getDatabase()->newTransaction(); __STP(trx);
+
+	insertRecord(trx, 1, L"tanaka", 11);
+	insertRecord(trx, 2, L"sato", 11);
+
+	trx->commit();
 }
 
 void TestDbSchemaAlterText01::insertRecord(CdbTransaction* trx, int id,	const wchar_t* name, int email_id) {
+	InsertLog* log = new InsertLog();
+
+	CdbTableIdentifier* tableId = new CdbTableIdentifier();
+	tableId->setTable(new UnicodeString(L"test_table"));
+	log->setTable(tableId);
+
+	CdbRecord* record = new CdbRecord();
+	LocalCdbOid loid(this->loidSerial++);
+	record->setOid(&loid);
+
+	record->addValue(new CdbIntValue(id));
+
+	record->addValue(new CdbStringValue(name));
+	record->addValue(new CdbIntValue(email_id));
+
+	log->addRecord(record);
+
+	trx->insert(log);
 }
 
 } /* namespace alinous */
