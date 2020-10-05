c34b948d1d16bca639ca4b6f7177542ba1f45f0b
iizuka <iizuka@alinous.org>
2020-10-05 20:25:08 +0900

    test case primary key is not unique

--- src_test/smartcontract_db/table_alter_primary/test_exec_alter_primary.cpp
+++ src_test/smartcontract_db/table_alter_primary/test_exec_alter_primary.cpp
@@ -29,6 +29,7 @@
 #include "table/CdbTableColumn.h"
 #include "table/CdbTableIndex.h"
 
+#include "../toolkit_alter/TestDbSchemaAlterTextUnique01.h"
 TEST_GROUP(TestExecAlterPrimaryGroup) {
 	TEST_SETUP() {
 		env->setup();
@@ -40,7 +41,7 @@ TEST_GROUP(TestExecAlterPrimaryGroup) {
 
 
 /**
- *
+ * primary key is unique
  */
 TEST(TestExecAlterPrimaryGroup, dropPrimaryKey01){
 	TestDbSchemaAlter01 tester(this->env);
@@ -69,6 +70,36 @@ TEST(TestExecAlterPrimaryGroup, dropPrimaryKey01){
 	}
 
 	// again
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		AlterTableStatement* stmt = lang->alterTableStatement(); __STP(stmt);
+		CHECK(!parser.hasError())
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		stmt->preAnalyze(actx);
+		stmt->analyzeTypeRef(actx);
+		stmt->analyze(actx);
+
+		stmt->interpret(vm);
+	}
+}
+
+/**
+ * primary key is not unique
+ */
+TEST(TestExecAlterPrimaryGroup, dropPrimaryKey02){
+	TestDbSchemaAlterTextUnique01 tester(this->env);
+	tester.init(1024*10);
+	tester.insert01();
+
+	VirtualMachine* vm = tester.getVm();
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/table_alter_primary/resources/exec_primary/dropPrimaryKey01.alns"))
 	{
 		SmartContractParser parser(sourceFile);
 		AlinousLang* lang = parser.getDebugAlinousLang();
