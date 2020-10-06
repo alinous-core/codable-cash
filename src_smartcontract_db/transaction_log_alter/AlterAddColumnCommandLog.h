5b354af1bfa85d13ddb24e292d1e507b651a2488
iizuka <iizuka@alinous.org>
2020-10-06 14:41:09 +0900

    AlterAddColumnCommand::interpret

--- src_smartcontract_db/transaction_log_alter/AlterAddColumnCommandLog.h
+++ src_smartcontract_db/transaction_log_alter/AlterAddColumnCommandLog.h
@@ -32,6 +32,10 @@ public:
 
 	virtual void initCommandParam(VirtualMachine* vm, TableIdentifier* tableId);
 
+	const AlterAddColumnCommand* getCommand() const noexcept {
+		return command;
+	}
+
 private:
 	AlterAddColumnCommand* command;
 };
