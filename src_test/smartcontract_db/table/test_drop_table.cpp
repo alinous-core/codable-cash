21db1fd191cdff9e959ac31c0b88fbc5ad5cae18
iizuka <iizuka@alinous.org>
2020-10-09 21:12:13 +0900

    TestDropTableGroup testbench

---
+++ src_test/smartcontract_db/table/test_drop_table.cpp
@@ -0,0 +1,53 @@
+/*
+ * test_drop_table.cpp
+ *
+ *  Created on: 2020/10/09
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "base/StackRelease.h"
+
+#include "base_io/File.h"
+
+#include "../toolkit/TestDbSchema01.h"
+
+#include "vm/VirtualMachine.h"
+
+#include "compiler/SmartContractParser.h"
+#include "alinous_lang/AlinousLang.h"
+
+#include "sql_ddl/DropTableStatement.h"
+
+using namespace codablecash;
+using namespace alinous;
+
+TEST_GROUP(TestDropTableGroup) {
+	TEST_SETUP() {
+		env->setup();
+	}
+	TEST_TEARDOWN() {
+		env->teardown();
+	}
+};
+
+/**
+ *	DROP TABLE test_table;
+ */
+TEST(TestDropTableGroup, case01){
+	TestDbSchema01 tester(this->env);
+	tester.init(1024*10);
+
+	VirtualMachine* vm = tester.getVm();
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/table/resources/droptable/case01.alns"))
+	{
+		SmartContractParser parser(sourceFile);
+		AlinousLang* lang = parser.getDebugAlinousLang();
+
+		DropTableStatement* stmt = lang->dropTableStatement(); __STP(stmt);
+		CHECK(!parser.hasError())
+	}
+}
