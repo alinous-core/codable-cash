971d7c345c87d69bd0ca662e975bf4cf72f417bd
iizuka <iizuka@alinous.org>
2020-10-13 14:54:14 +0900

    moved smartcontract engine

--- sh/javacc.sh
+++ sh/javacc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -eux
 
-if test -e src_smartcontract/jj/alinous.jj ; then
-  rm src_smartcontract/jj/alinous.jj
+if test -e src_smartcontract/engine/jj/alinous.jj ; then
+  rm src_smartcontract/engine/jj/alinous.jj
 fi
 
-cat src_smartcontract/jj/header.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/lex.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/base.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/types.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/statement.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/statement_ctrl.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/expression.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/sql_ddl.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/sql_dml.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/sql_expression.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/sql_join.jj >> src_smartcontract/jj/alinous.jj
-cat src_smartcontract/jj/json.jj >> src_smartcontract/jj/alinous.jj
+cat src_smartcontract/engine/jj/header.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/lex.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/base.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/types.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/statement.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/statement_ctrl.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/expression.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/sql_ddl.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/sql_dml.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/sql_expression.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/sql_join.jj >> src_smartcontract/engine/jj/alinous.jj
+cat src_smartcontract/engine/jj/json.jj >> src_smartcontract/engine/jj/alinous.jj
 
-java -classpath ./tools/javacc-7.0.4.jar javacc -STATIC=false ./src_smartcontract/jj/alinous.jj
-#java -classpath ./tools/javacc-7.0.4.jar javacc -STATIC=false -LOOKAHEAD:3 ./src_smartcontract/jj/alinous.jj
+java -classpath ./tools/javacc-7.0.4.jar javacc -STATIC=false ./src_smartcontract/engine/jj/alinous.jj
+#java -classpath ./tools/javacc-7.0.4.jar javacc -STATIC=false -LOOKAHEAD:3 ./src_smartcontract/engine/jj/alinous.jj
 
