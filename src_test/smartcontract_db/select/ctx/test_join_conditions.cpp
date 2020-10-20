1d3a10c7317eea872e1d4386662b3bff72cceb2d
iizuka <iizuka@alinous.org>
2020-10-20 18:18:18 +0900

    TestJoinConditionGroup

---
+++ src_test/smartcontract_db/select/ctx/test_join_conditions.cpp
@@ -0,0 +1,23 @@
+/*
+ * test_join_conditions.cpp
+ *
+ *  Created on: 2020/10/20
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+
+
+TEST_GROUP(TestJoinConditionGroup) {
+	TEST_SETUP(){
+		env->setup();
+	}
+	TEST_TEARDOWN(){
+		env->teardown();
+	}
+};
+
+TEST(TestJoinConditionGroup, case01){
+
+}
