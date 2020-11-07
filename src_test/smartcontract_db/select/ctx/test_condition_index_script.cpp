a029f165fea73acd37828fae06b4fe2f3b11c6fe
iizuka <iizuka@alinous.org>
2020-11-07 22:27:27 +0900

    TestConditionIndexScriptGroup

---
+++ src_test/smartcontract_db/select/ctx/test_condition_index_script.cpp
@@ -0,0 +1,26 @@
+/*
+ * test_condition_index_script.cpp
+ *
+ *  Created on: 2020/11/07
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+
+#include "base/StackRelease.h"
+
+//using namespace codablecash;
+
+TEST_GROUP(TestConditionIndexScriptGroup) {
+	TEST_SETUP(){
+		env->setup();
+	}
+	TEST_TEARDOWN(){
+		env->teardown();
+	}
+};
+
+TEST(TestConditionIndexScriptGroup, case01){
+
+}
