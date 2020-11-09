a46b700d2313c72ef5aefb2faa2cadff060a913d
iizuka <iizuka@alinous.org>
2020-11-09 23:01:09 +0900

    parse condition

--- src_test/smartcontract_db/select/ctx/test_condition_index_script.cpp
+++ src_test/smartcontract_db/select/ctx/test_condition_index_script.cpp
@@ -10,6 +10,17 @@
 
 #include "base/StackRelease.h"
 
+#include "base_io/File.h"
+
+#include "lang_sql/sql/AbstractSQLExpression.h"
+
+#include "engine/compiler/SmartContractParser.h"
+#include "engine/sc_analyze/AnalyzeContext.h"
+
+#include "alinous_lang/AlinousLang.h"
+#include "smartcontract_db/toolkit/TestDbSchema01.h"
+
+
 //using namespace codablecash;
 
 TEST_GROUP(TestConditionIndexScriptGroup) {
@@ -21,6 +32,32 @@ TEST_GROUP(TestConditionIndexScriptGroup) {
 	}
 };
 
-TEST(TestConditionIndexScriptGroup, case01){
+static AbstractSQLExpression* parse(const File* sourceFile);
 
+AbstractSQLExpression* parse(const File* sourceFile){
+	SmartContractParser parser(sourceFile);
+	AlinousLang* lang = parser.getDebugAlinousLang();
+
+	return lang->sqlExpression();
+}
+
+TEST(TestConditionIndexScriptGroup, case01){
+	TestDbSchema01 tester(this->env);
+	tester.init(1024 * 10);
+
+	VirtualMachine* vm = tester.getVm();
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/select/ctx/resources/conditions/and01.alns"))
+
+	{
+		AbstractSQLExpression* cond = parse(sourceFile); __STP(cond);
+
+		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+		actx->setVm(vm);
+
+		cond->preAnalyze(actx);
+		cond->analyzeTypeRef(actx);
+		cond->analyze(actx);
+	}
 }
