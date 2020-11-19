588dbdf926f7e38138497ac895d130708d7efa80
iizuka <iizuka@alinous.org>
2020-11-19 23:20:27 +0900

    IndexRangeCandidate::toCodeString()

--- src_smartcontract_db/scan_select/scan_planner/scanner/index/IndexRangeCandidate.cpp
+++ src_smartcontract_db/scan_select/scan_planner/scanner/index/IndexRangeCandidate.cpp
@@ -7,6 +7,10 @@
 
 #include "scan_select/scan_planner/scanner/index/IndexRangeCandidate.h"
 
+#include "scan_select/scan_condition/params/ColumnIdentifierScanParam.h"
+
+#include "base/UnicodeString.h"
+
 namespace codablecash {
 
 IndexRangeCandidate::IndexRangeCandidate() : IndexCandidate(IndexType::RANGE) {
@@ -17,6 +21,44 @@ IndexRangeCandidate::IndexRangeCandidate() : IndexCandidate(IndexType::RANGE) {
 
 IndexRangeCandidate::~IndexRangeCandidate() {
 	this->topValue = nullptr;
+}
+
+const UnicodeString* IndexRangeCandidate::toCodeString() noexcept {
+	if(this->str == nullptr){
+		this->str = new UnicodeString();
+
+		IValueProvider* vp = const_cast<IValueProvider*>(this->value);
+		const UnicodeString* s = vp->toStringCode();
+
+		this->str->append(s);
+
+		if(this->bottomEq){
+			this->str->append(L" < ");
+		}
+		else {
+			this->str->append(L" <= ");
+		}
+
+		ColumnIdentifierScanParam* param = const_cast<ColumnIdentifierScanParam*>(this->column);
+
+		s = param->toStringCode();
+		this->str->append(s);
+
+		if(this->topEq){
+			this->str->append(L" < ");
+		}
+		else {
+			this->str->append(L" <= ");
+		}
+
+		// top value
+		vp = const_cast<IValueProvider*>(this->topValue);
+		s = vp->toStringCode();
+
+		this->str->append(s);
+	}
+
+	return this->str;
 }
 
 } /* namespace codablecash */
--- src_smartcontract_db/scan_select/scan_planner/scanner/index/IndexRangeCandidate.h
+++ src_smartcontract_db/scan_select/scan_planner/scanner/index/IndexRangeCandidate.h
@@ -19,6 +19,8 @@ public:
 	IndexRangeCandidate();
 	virtual ~IndexRangeCandidate();
 
+	virtual const UnicodeString* toCodeString() noexcept;
+
 	void setBottomEq(bool bottomEq) noexcept {
 		this->bottomEq = bottomEq;
 	}
