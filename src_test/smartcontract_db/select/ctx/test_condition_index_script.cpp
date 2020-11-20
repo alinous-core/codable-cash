e7c2c25b415582007e6de98301271a5ec698faab
iizuka <iizuka@alinous.org>
2020-11-20 17:04:33 +0900

    simplify test

--- src_test/smartcontract_db/select/ctx/test_condition_index_script.cpp
+++ src_test/smartcontract_db/select/ctx/test_condition_index_script.cpp
@@ -61,6 +61,56 @@ AbstractSQLExpression* parse(const File* sourceFile){
 	return lang->sqlExpression();
 }
 
+static UnicodeString* getCandidate(const File* sourceFile, VirtualMachine* vm, TestDbSchemaBase& tester);
+static UnicodeString* getCandidate(const File* sourceFile, VirtualMachine* vm, TestDbSchemaBase& tester){
+	AbstractSQLExpression* sqlexp = parse(sourceFile); __STP(sqlexp);
+
+	AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
+	actx->setVm(vm);
+
+	sqlexp->preAnalyze(actx);
+	sqlexp->analyzeTypeRef(actx);
+	sqlexp->analyze(actx);
+
+	sqlexp->init(vm);
+
+	SelectScanPlanner* planner = new SelectScanPlanner(); __STP(planner);
+	VmSelectPlannerSetter setter(vm, planner);
+
+	TablesHolder* tableHolder = planner->getTablesHolder();
+	TableScanTarget* target = tester.getScanTarget(L"public", L"test_table");
+	tableHolder->addScanTarget(target);
+
+	sqlexp->interpret(vm);
+
+	ConditionsHolder* cholder = planner->getConditions();
+	RootScanCondition* root = cholder->getRoot();
+
+	AbstractScanConditionElement* element = cholder->pop();
+
+	AbstractScanCondition* cond = ScanConditionCast::toAbstractScanCondition(element, vm, nullptr);
+	root->addCondition(cond);
+	root->analyzeConditions(vm, planner);
+
+	FilterConditionDitector filterDetector(vm, planner);
+	filterDetector.detect(target);
+
+	AbstractScanCondition* filterCondition = filterDetector.getCondition();
+
+	TableIndexDetector indexDetextor(vm, planner, target);
+	indexDetextor.detect(filterCondition);
+
+	AbstractIndexCandidate* candidate = nullptr;
+	if(!indexDetextor.isEmpty()){
+		candidate = indexDetextor.pop();
+	}
+	__STP(candidate);
+
+	const UnicodeString* str = candidate->toCodeString();
+
+	return new UnicodeString(str);
+}
+
 TEST(TestConditionIndexScriptGroup, case01){
 	TestDbSchema01 tester(this->env);
 	tester.init(1024 * 10);
@@ -71,50 +121,8 @@ TEST(TestConditionIndexScriptGroup, case01){
 	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/select/ctx/resources/conditions/and01.alns"))
 
 	{
-		AbstractSQLExpression* sqlexp = parse(sourceFile); __STP(sqlexp);
+		UnicodeString* str = getCandidate(sourceFile, vm, tester); __STP(str);
 
-		AnalyzeContext* actx = new AnalyzeContext(); __STP(actx);
-		actx->setVm(vm);
-
-		sqlexp->preAnalyze(actx);
-		sqlexp->analyzeTypeRef(actx);
-		sqlexp->analyze(actx);
-
-		sqlexp->init(vm);
-
-
-		SelectScanPlanner* planner = new SelectScanPlanner(); __STP(planner);
-		VmSelectPlannerSetter setter(vm, planner);
-
-		TablesHolder* tableHolder = planner->getTablesHolder();
-		TableScanTarget* target = tester.getScanTarget(L"public", L"test_table");
-		tableHolder->addScanTarget(target);
-
-		sqlexp->interpret(vm);
-
-		ConditionsHolder* cholder = planner->getConditions();
-		RootScanCondition* root = cholder->getRoot();
-
-		AbstractScanConditionElement* element = cholder->pop();
-
-		AbstractScanCondition* cond = ScanConditionCast::toAbstractScanCondition(element, vm, nullptr);
-		root->addCondition(cond);
-		root->analyzeConditions(vm, planner);
-
-		FilterConditionDitector filterDetector(vm, planner);
-		filterDetector.detect(target);
-
-		AbstractScanCondition* filterCondition = filterDetector.getCondition();
-
-		TableIndexDetector indexDetextor(vm, planner, target);
-		indexDetextor.detect(filterCondition);
-
-		AbstractIndexCandidate* candidate = nullptr;
-		if(!indexDetextor.isEmpty()){
-			candidate = indexDetextor.pop();
-		}
-
-		const UnicodeString* str = candidate->toCodeString();
 		UnicodeString ans(L"0 <= test_table.id <= 100");
 		CHECK(ans.equals(str));
 	}
