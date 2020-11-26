64d8cb9e1ddd4cae7f8f9e96a15789da50fe212a
iizuka <iizuka@alinous.org>
2020-11-26 13:43:55 +0900

    IndexResolver

---
+++ src_smartcontract_db/scan_select/scan_planner/scanner/index/IndexResolver.cpp
@@ -0,0 +1,20 @@
+/*
+ * IndexResolver.cpp
+ *
+ *  Created on: 2020/11/26
+ *      Author: iizuka
+ */
+
+#include "scan_select/scan_planner/scanner/index/IndexResolver.h"
+
+namespace codablecash {
+
+IndexResolver::IndexResolver() {
+
+}
+
+IndexResolver::~IndexResolver() {
+
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/scan_select/scan_planner/scanner/index/IndexResolver.h
@@ -0,0 +1,21 @@
+/*
+ * IndexResolver.h
+ *
+ *  Created on: 2020/11/26
+ *      Author: iizuka
+ */
+
+#ifndef SCAN_SELECT_SCAN_PLANNER_SCANNER_INDEX_INDEXRESOLVER_H_
+#define SCAN_SELECT_SCAN_PLANNER_SCANNER_INDEX_INDEXRESOLVER_H_
+
+namespace codablecash {
+
+class IndexResolver {
+public:
+	IndexResolver();
+	virtual ~IndexResolver();
+};
+
+} /* namespace codablecash */
+
+#endif /* SCAN_SELECT_SCAN_PLANNER_SCANNER_INDEX_INDEXRESOLVER_H_ */
