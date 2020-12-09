f10d1e071f38f6d26ae4141deff109b2254ecf8f
iizuka <iizuka@alinous.org>
2020-12-09 23:49:48 +0900

    TestIndexResolverScriptGroup

---
+++ src_test/smartcontract_db/select/ctx/test_index_resolver_script.cpp
@@ -0,0 +1,25 @@
+/*
+ * test_index_resolver_script.cpp
+ *
+ *  Created on: 2020/12/09
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+
+#include "base/StackRelease.h"
+
+
+TEST_GROUP(TestIndexResolverScriptGroup) {
+	TEST_SETUP(){
+		env->setup();
+	}
+	TEST_TEARDOWN(){
+		env->teardown();
+	}
+};
+
+TEST(TestIndexResolverScriptGroup, case01){
+
+}
