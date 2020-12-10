4c577ef93dbf584f5fd50c392841afd50a7cbf3b
iizuka <iizuka@alinous.org>
2020-12-10 13:45:18 +0900

    test AND index

--- src_test/smartcontract_db/select/ctx/test_index_resolver_script.cpp
+++ src_test/smartcontract_db/select/ctx/test_index_resolver_script.cpp
@@ -133,3 +133,19 @@ TEST(TestIndexResolverScriptGroup, case01){
 		CHECK(ans.equals(str));
 	}
 }
+
+TEST(TestIndexResolverScriptGroup, case02){
+	TestDbSchema01 tester(this->env);
+	tester.init(1024 * 10);
+
+	VirtualMachine* vm = tester.getVm();
+
+	const File* projectFolder = this->env->getProjectRoot();
+	_ST(File, sourceFile, projectFolder->get(L"src_test/smartcontract_db/select/ctx/resources/index_resolver/cond02.alns"))
+	{
+		UnicodeString* str = getCandidate(sourceFile, vm, tester); __STP(str);
+
+		UnicodeString ans(L"0 < id AND email_id = 100");
+		CHECK(ans.equals(str));
+	}
+}
