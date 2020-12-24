38067002cd5e0d90396e841cbb391b00dfa81233
iizuka <iizuka@alinous.org>
2020-12-24 18:46:53 +0900

    RightTableBufferedTransactionScanner

--- src_smartcontract_db/trx/scan/transaction_scanner_join_right/RightTableBufferedTransactionScanner.cpp
+++ src_smartcontract_db/trx/scan/transaction_scanner_join_right/RightTableBufferedTransactionScanner.cpp
@@ -7,15 +7,18 @@
 
 #include "trx/scan/transaction_scanner_join_right/RightTableBufferedTransactionScanner.h"
 
+#include "scan_select/scan_planner/scanner/join/AbstractJoinCandidate.h"
+
 namespace codablecash {
 
 RightTableBufferedTransactionScanner::RightTableBufferedTransactionScanner(
 		ScanResultMetadata* metadata, CdbTransaction* trx,
 		const CdbTable* table, AbstractJoinCandidate* joinCandidate) : AbstractTransactionScanner(metadata, trx, table) {
+	this->joinCandidate = joinCandidate->copy();
 }
 
 RightTableBufferedTransactionScanner::~RightTableBufferedTransactionScanner() {
-
+	delete this->joinCandidate;
 }
 
 void RightTableBufferedTransactionScanner::start() {
@@ -25,9 +28,11 @@ void RightTableBufferedTransactionScanner::reset(const AbstractCdbKey* key) {
 }
 
 bool RightTableBufferedTransactionScanner::hasNext() {
+	return false;
 }
 
 const CdbRecord* RightTableBufferedTransactionScanner::next() {
+	return nullptr;
 }
 
 void RightTableBufferedTransactionScanner::shutdown() {
