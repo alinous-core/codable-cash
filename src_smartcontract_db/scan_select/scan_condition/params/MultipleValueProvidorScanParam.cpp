772ed4ea81c6be68bbdc460f7648f95872a6f017
iizuka <iizuka@alinous.org>
2021-01-12 12:14:27 +0900

    added MultipleValueProvidorScanParam class

---
+++ src_smartcontract_db/scan_select/scan_condition/params/MultipleValueProvidorScanParam.cpp
@@ -0,0 +1,21 @@
+/*
+ * MultipleValueProvidorScanParam.cpp
+ *
+ *  Created on: 2021/01/12
+ *      Author: iizuka
+ */
+
+#include "scan_select/scan_condition/params/MultipleValueProvidorScanParam.h"
+
+namespace codablecash {
+
+MultipleValueProvidorScanParam::MultipleValueProvidorScanParam() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MultipleValueProvidorScanParam::~MultipleValueProvidorScanParam() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/scan_select/scan_condition/params/MultipleValueProvidorScanParam.h
@@ -0,0 +1,21 @@
+/*
+ * MultipleValueProvidorScanParam.h
+ *
+ *  Created on: 2021/01/12
+ *      Author: iizuka
+ */
+
+#ifndef SCAN_SELECT_SCAN_CONDITION_PARAMS_MULTIPLEVALUEPROVIDORSCANPARAM_H_
+#define SCAN_SELECT_SCAN_CONDITION_PARAMS_MULTIPLEVALUEPROVIDORSCANPARAM_H_
+
+namespace codablecash {
+
+class MultipleValueProvidorScanParam {
+public:
+	MultipleValueProvidorScanParam();
+	virtual ~MultipleValueProvidorScanParam();
+};
+
+} /* namespace codablecash */
+
+#endif /* SCAN_SELECT_SCAN_CONDITION_PARAMS_MULTIPLEVALUEPROVIDORSCANPARAM_H_ */
