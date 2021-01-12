52f7bc6ca94ae1b61ae124c72bc92fcd1f934071
iizuka <iizuka@alinous.org>
2021-01-12 18:30:27 +0900

    MultipleValueProvidorScanParam

--- src_smartcontract_db/scan_select/scan_condition/params/MultipleValueProvidorScanParam.cpp
+++ src_smartcontract_db/scan_select/scan_condition/params/MultipleValueProvidorScanParam.cpp
@@ -7,15 +7,50 @@
 
 #include "scan_select/scan_condition/params/MultipleValueProvidorScanParam.h"
 
+#include "scan_select/scan_condition/base/IValueProvider.h"
+
+#include "base/UnicodeString.h"
+
 namespace codablecash {
 
-MultipleValueProvidorScanParam::MultipleValueProvidorScanParam() {
-	// TODO Auto-generated constructor stub
+MultipleValueProvidorScanParam::MultipleValueProvidorScanParam(const MultipleValueProvidorScanParam& inst) {
+	this->str = nullptr;
+}
 
+MultipleValueProvidorScanParam::MultipleValueProvidorScanParam() {
+	this->str = nullptr;
 }
 
 MultipleValueProvidorScanParam::~MultipleValueProvidorScanParam() {
-	// TODO Auto-generated destructor stub
+	this->list.deleteElements();
+	delete this->str;
+}
+
+const UnicodeString* MultipleValueProvidorScanParam::toStringCode() noexcept {
+	if(this->str == nullptr){
+		this->str = new UnicodeString(L"");
+
+		int maxLoop = this->list.size();
+		for(int i = 0; i != maxLoop; ++i){
+			IValueProvider* vp = this->list.get(i);
+
+			const UnicodeString* s = vp->toStringCode();
+			// FIXME　MultipleValueProvidorScanParam::toStringCode()
+		}
+	}
+
+	return this->str;
+}
+
+void MultipleValueProvidorScanParam::analyzeConditions(VirtualMachine* vm, SelectScanPlanner* planner) {
+	int maxLoop = this->list.size();
+	for(int i = 0; i != maxLoop; ++i){
+		IValueProvider* vp = this->list.get(i);
+	}
+}
+
+IValueProvider* MultipleValueProvidorScanParam::clone() const noexcept {
+	return new MultipleValueProvidorScanParam(*this);
 }
 
 } /* namespace codablecash */
--- src_smartcontract_db/scan_select/scan_condition/params/MultipleValueProvidorScanParam.h
+++ src_smartcontract_db/scan_select/scan_condition/params/MultipleValueProvidorScanParam.h
@@ -8,12 +8,29 @@
 #ifndef SCAN_SELECT_SCAN_CONDITION_PARAMS_MULTIPLEVALUEPROVIDORSCANPARAM_H_
 #define SCAN_SELECT_SCAN_CONDITION_PARAMS_MULTIPLEVALUEPROVIDORSCANPARAM_H_
 
+#include "scan_select/scan_condition/params/AbstractScanConditionParameter.h"
+
+#include "base/ArrayList.h"
+
 namespace codablecash {
 
-class MultipleValueProvidorScanParam {
+class IValueProvider;
+
+class MultipleValueProvidorScanParam : public AbstractScanConditionParameter {
 public:
+	MultipleValueProvidorScanParam(const MultipleValueProvidorScanParam& inst);
 	MultipleValueProvidorScanParam();
 	virtual ~MultipleValueProvidorScanParam();
+
+	virtual void analyzeConditions(VirtualMachine* vm, SelectScanPlanner* planner);
+
+	virtual const UnicodeString* toStringCode() noexcept;
+	virtual IValueProvider* clone() const noexcept;
+
+private:
+	ArrayList<IValueProvider> list;
+
+	UnicodeString* str;
 };
 
 } /* namespace codablecash */
